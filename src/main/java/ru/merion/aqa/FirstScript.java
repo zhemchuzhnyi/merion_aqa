720ea3dffd0cae462468c6758469cd8d5af8dc28
Andrey_O <a_reshkin@inbox.ru>
2025-05-21 23:44:11 +0300

    First AutoScript

--- src/main/java/ru/merion/aqa/FirstScript.java
+++ src/main/java/ru/merion/aqa/FirstScript.java
@@ -1,8 +1,14 @@
 package ru.merion.aqa;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class FirstScript {
 
     public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://google.ru");
 
 
 
