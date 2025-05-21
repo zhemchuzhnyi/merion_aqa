e8938ed21ea083720b17f17231d6f511d8470cb4
Andrey_O <a_reshkin@inbox.ru>
2025-05-22 02:07:42 +0300

    Update Script + Create Factory + Update Options

--- src/main/java/ru/merion/aqa/FirstScript.java
+++ src/main/java/ru/merion/aqa/FirstScript.java
@@ -2,13 +2,19 @@ package ru.merion.aqa;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.io.File;
 
 public class FirstScript {
 
     public static void main(String[] args) {
 
-        WebDriver driver1 = WebDriverFactory.create("opera");
-        driver1.get("https:google.com");
+        ChromeOptions options = new ChromeOptions();
+        options.addExtensions(new File("/Users/a0000/IdeaProjects/merion_aqa/src/main/resources/User-Agent-Switcher-for-Chrome-Chrome.crx"));
+
+        WebDriver driver1 = WebDriverFactory.create(options);
+        driver1.get("https://ya.ru");
 
 
 
--- src/main/java/ru/merion/aqa/WebDriverFactory.java
+++ src/main/java/ru/merion/aqa/WebDriverFactory.java
@@ -2,29 +2,45 @@ package ru.merion.aqa;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.safari.SafariOptions;
 
 public class WebDriverFactory {
 
     public static WebDriver create(String browserName) {
-
         if (browserName.equalsIgnoreCase("chrome")) {
-            return new ChromeDriver();
+            return create( new ChromeOptions());
         }
         if (browserName.equalsIgnoreCase("firefox")) {
-            return new FirefoxDriver();
+            return create(new FirefoxOptions());
         }
-
         if (browserName.equalsIgnoreCase("edge")) {
-            return new EdgeDriver();
+            return create(new EdgeOptions());
         }
-
         if (browserName.equalsIgnoreCase("safari")) {
-            return new SafariDriver();
+            return create(new SafariOptions());
         }
+        throw new IllegalArgumentException("Неподдерживаемый тип браузера " + browserName + ". (chrome|firefox|edge|safari)");
+    }
 
-        throw new IllegalArgumentException("Неподдерживаемый тип браузера " + browserName + ". [chrome|firefox|edge|safari]");
+    public static WebDriver create(SafariOptions options) {
+        return new SafariDriver(options);
+    }
+
+    public static WebDriver create(FirefoxOptions options) {
+        return new FirefoxDriver(options);
+    }
+
+    public static WebDriver create(ChromeOptions options) {
+        return new ChromeDriver(options);
+    }
+
+    public static WebDriver create(EdgeOptions options) {
+        return new EdgeDriver(options);
     }
 }
