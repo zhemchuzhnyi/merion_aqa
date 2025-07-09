5c54f1e905eb16be451e9048d9013135d2d5b679
Andrey_O <a_reshkin@inbox.ru>
2025-07-09 21:50:49 +0300

    Update Project: - GetUrl

---
+++ src/main/java/ru/merion/aqa/lesson3/GetUrl.java
@@ -0,0 +1,16 @@
+package ru.merion.aqa.lesson3;
+
+import org.openqa.selenium.WebDriver;
+import ru.merion.aqa.WebDriverFactory;
+
+public class GetUrl {
+    public static void main(String[] args) {
+
+        WebDriver driver = WebDriverFactory.create("chrome");
+
+        driver.get("https://habr.com/ru");
+
+        driver.quit();
+
+    }
+}
