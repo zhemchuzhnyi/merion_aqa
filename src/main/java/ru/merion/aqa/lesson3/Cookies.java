9a55b74bf8255b8279cac0ad5525af6d2f3e040b
Andrey_O <a_reshkin@inbox.ru>
2025-07-15 22:52:36 +0300

    Update Project: - Cookie

--- src/main/java/ru/merion/aqa/lesson3/Cookies.java
+++ src/main/java/ru/merion/aqa/lesson3/Cookies.java
@@ -1,17 +1,21 @@
 package ru.merion.aqa.lesson3;
 
+import org.openqa.selenium.Cookie;
 import org.openqa.selenium.WebDriver;
 import ru.merion.aqa.WebDriverFactory;
+
+import java.util.Date;
 
 public class Cookies {
 
     public static void main(String[] args) {
 
         WebDriver driver = WebDriverFactory.create("chrome");
-        driver.get("https://www.labirint.ru/");
-        String currentUrl = driver.getCurrentUrl();
-        String title = driver.getTitle();
-        String pageSource = driver.getPageSource();
+
+        Cookie cookie = new Cookie("cookie_policy", "1", "labirint.ru", "/", new Date(2025, 07, 15));
+        driver.manage().addCookie(cookie);
+        driver.get("http://www.labirint.ru/");
+
 
 
         driver.quit();
