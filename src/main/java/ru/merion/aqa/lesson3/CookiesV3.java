815ec7cbc0f78261c6c90abb8c68cdf65c76a3a2
Andrey_O <a_reshkin@inbox.ru>
2025-07-17 22:52:52 +0300

    Update Project: - [CookiesV3 upgrade and refactor]

--- src/main/java/ru/merion/aqa/lesson3/CookiesV3.java
+++ src/main/java/ru/merion/aqa/lesson3/CookiesV3.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.Cookie;
 import org.openqa.selenium.WebDriver;
 import ru.merion.aqa.WebDriverFactory;
 
+import java.util.Set;
+
 public class CookiesV3 {
 
     public static void main(String[] args) {
@@ -13,7 +15,7 @@ public class CookiesV3 {
         driver.get("https://lms.merionet.ru");
 
             // Добавляем куки
-        Cookie cookie = new Cookie("MoodleSession", "");
+        Cookie cookie = new Cookie("MoodleSession", "da8k3stv0052052efj1p4t0pn4");
         driver.manage().addCookie(cookie);
 
             // Обновляем страницу, чтобы куки применились
@@ -21,6 +23,9 @@ public class CookiesV3 {
 
             // Переходим на страницу личного кабинета
         driver.get("https://lms.merionet.ru/my/");
+
+        Set<Cookie> cookies = driver.manage().getCookies();
+        Cookie MoodleSession = driver.manage().getCookieNamed("MoodleSession");
 
         driver.quit();
     }
