af252c256bf9245e9ee4ee3ae41b085d9aed3b51
Andrey_O <a_reshkin@inbox.ru>
2025-07-21 23:01:09 +0300

    Update Project: - [Switch]

---
+++ src/main/java/ru/merion/aqa/lesson3/Switch.java
@@ -0,0 +1,17 @@
+package ru.merion.aqa.lesson3;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import ru.merion.aqa.WebDriverFactory;
+
+public class Switch {
+
+    public static void main(String[] args) {
+
+        WebDriver driver = WebDriverFactory.create("chrome");
+
+
+
+    }
+
+}
