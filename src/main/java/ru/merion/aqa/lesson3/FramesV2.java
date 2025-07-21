506614220dc884f81a8a3a4f735d31555f2f10ec
Andrey_O <a_reshkin@inbox.ru>
2025-07-22 01:03:39 +0300

    Update Project: - [iFramesV2]

---
+++ src/main/java/ru/merion/aqa/lesson3/FramesV2.java
@@ -0,0 +1,29 @@
+package ru.merion.aqa.lesson3;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import ru.merion.aqa.WebDriverFactory;
+
+import java.util.List;
+
+
+public class FramesV2 {
+
+    public static void main(String[] args) {
+
+        WebDriver driver = WebDriverFactory.create("chrome");
+        driver.get("https://the-internet.herokuapp.com/nested_frames");
+        List<WebElement> shouldBeEmpty = driver.findElements(By.cssSelector("body"));
+
+        driver.switchTo().frame("frame-top");
+        driver.switchTo().frame("frame-midddle");
+        List<WebElement> shouldHaveBody = driver.findElements(By.cssSelector("body"));
+
+        driver.quit();
+
+
+
+
+    }
+}
