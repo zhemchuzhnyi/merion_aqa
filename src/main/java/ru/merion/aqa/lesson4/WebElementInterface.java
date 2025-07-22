8bfdedd1d8215bc4e07aa509101835a29372f53a
Andrey_O <a_reshkin@inbox.ru>
2025-07-23 00:03:23 +0300

    Update Project: - [WebElements on page]

--- src/main/java/ru/merion/aqa/lesson4/WebElementInterface.java
+++ src/main/java/ru/merion/aqa/lesson4/WebElementInterface.java
@@ -1,11 +1,16 @@
 package ru.merion.aqa.lesson4;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import ru.merion.aqa.WebDriverFactory;
 
 public class WebElementInterface {
     public static void main(String[] args) {
        WebDriver driver = WebDriverFactory.create("chrome");
        driver.get("https://habr.com/ru");
+
+       WebElement element = driver.findElement(By.cssSelector(".tm-header-user-menu__login"));
+
     }
 }
