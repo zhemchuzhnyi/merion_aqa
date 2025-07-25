6d7c6afd17b58d824b7bef5908c20fe0c5434b36
Andrey_O <a_reshkin@inbox.ru>
2025-07-25 23:40:21 +0300

    Update Project: - [find WebElements on page Habr]

--- src/main/java/ru/merion/aqa/lesson4/WebElementInterface.java
+++ src/main/java/ru/merion/aqa/lesson4/WebElementInterface.java
@@ -17,7 +17,7 @@ public class WebElementInterface {
 
        //навигация на странице
        WebElement nav = driver.findElement(By.cssSelector(".tm-tabs__scroll-area"));
-       List<WebElement> links = nav.findElements(By.cssSelector("span"));
+       List<WebElement> links = nav.findElements(By.cssSelector("a"));
        System.out.println(links.size());
 
        driver.quit();
