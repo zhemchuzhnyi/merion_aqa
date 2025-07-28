5a946440f35feeea090e4ff9098a585c18c7f74a
Andrey_O <a_reshkin@inbox.ru>
2025-07-28 23:41:12 +0300

    Update Project: - [Click]

---
+++ src/main/java/ru/merion/aqa/lesson4/Click.java
@@ -0,0 +1,12 @@
+package ru.merion.aqa.lesson4;
+
+import org.openqa.selenium.WebDriver;
+import ru.merion.aqa.WebDriverFactory;
+
+public class Click {
+    public static void main(String[] args) {
+
+        WebDriver driver = WebDriverFactory.create("chrome");
+
+    }
+}
