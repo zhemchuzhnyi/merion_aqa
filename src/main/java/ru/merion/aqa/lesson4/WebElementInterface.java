6d0cd15c174179233a2f0f06aaf1a7542475b04b
Andrey_O <a_reshkin@inbox.ru>
2025-07-28 23:39:12 +0300

    Update Project: - [find WebElements on page Habr]

--- src/main/java/ru/merion/aqa/lesson4/WebElementInterface.java
+++ src/main/java/ru/merion/aqa/lesson4/WebElementInterface.java
@@ -1,10 +1,12 @@
 package ru.merion.aqa.lesson4;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import ru.merion.aqa.WebDriverFactory;
 
+import java.io.File;
 import java.util.List;
 
 public class WebElementInterface {
@@ -19,6 +21,8 @@ public class WebElementInterface {
        WebElement nav = driver.findElement(By.cssSelector(".tm-tabs__scroll-area"));
        List<WebElement> links = nav.findElements(By.cssSelector("a"));
        System.out.println(links.size());
+
+       login.getScreenshotAs(OutputType.FILE).renameTo(new File("btn.png"));
 
        driver.quit();
 
