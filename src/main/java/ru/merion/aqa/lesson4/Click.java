215487ed8a7b2022f1b30873c0234b93ea734815
Andrey_O <a_reshkin@inbox.ru>
2025-07-29 00:06:49 +0300

    Update Project: - [Click]

--- src/main/java/ru/merion/aqa/lesson4/Click.java
+++ src/main/java/ru/merion/aqa/lesson4/Click.java
@@ -1,12 +1,21 @@
 package ru.merion.aqa.lesson4;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import ru.merion.aqa.WebDriverFactory;
 
 public class Click {
     public static void main(String[] args) {
 
         WebDriver driver = WebDriverFactory.create("chrome");
+
+        driver.get("https://habr.com/ru");
+        WebElement login = driver.findElement(By.cssSelector(".tm-header-user-menu__login"));
+        login.click();
+
+
+        driver.quit();
 
     }
 }
