56a9f422d055ed30d1bc70a996dc1c9c67751bc0
Andrey_O <a_reshkin@inbox.ru>
2025-07-30 23:16:45 +0300

    Update Project: - [SendKeys - ввод текста с клавиатуры]

--- src/main/java/ru/merion/aqa/lesson4/SendKeys.java
+++ src/main/java/ru/merion/aqa/lesson4/SendKeys.java
@@ -1,6 +1,7 @@
 package ru.merion.aqa.lesson4;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import ru.merion.aqa.WebDriverFactory;
 
@@ -10,7 +11,10 @@ public class SendKeys {
 
         WebDriver driver = WebDriverFactory.create("chrome");
         driver.get("http://uitestingplayground.com/textinput");
-        driver.findElement(By.cssSelector("#newButtonName")).sendKeys("Hello!");
+        driver.findElement(By.cssSelector("#newButtonName")).sendKeys("Welcome!");
+        driver.findElement(By.cssSelector("#newButtonName")).sendKeys(" To site!");
+        driver.findElement(By.cssSelector("#newButtonName")).sendKeys(Keys.chord(Keys.LEFT_SHIFT, Keys.ARROW_UP)); // выделяем текс
+        driver.findElement(By.cssSelector("#newButtonName")).sendKeys(Keys.BACK_SPACE); // убирает последний символ (восклицательный знак уберет) если все выделено - удалит
         driver.findElement(By.cssSelector("#updatingButton")).click();
 
         driver.quit();
