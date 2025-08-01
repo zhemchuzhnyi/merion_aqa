15d0f3ed0db70942894073f7b113e5cc14c506b2
Andrey_O <a_reshkin@inbox.ru>
2025-08-01 18:51:09 +0300

    Update Project: - [Properties]

--- src/main/java/ru/merion/aqa/lesson4/Properties.java
+++ src/main/java/ru/merion/aqa/lesson4/Properties.java
@@ -11,7 +11,7 @@ public class Properties {
         WebDriver driver = WebDriverFactory.create("chrome");
         driver.get("https://habr.com/ru/feed/");
 
-        String target1 = driver.findElement(By.cssSelector(".tm-article-title__link")).getAttribute("clientHeight");
+        String target1 = driver.findElement(By.cssSelector(".tm-article-title__link")).getAttribute("contentEditable");
         String target2 = driver.findElement(By.cssSelector(".tm-article-title__link")).getDomProperty("clientHeight");
         String target3 = driver.findElement(By.cssSelector(".tm-article-title__link")).getDomAttribute("clientHeight");
         String target4 = driver.findElement(By.cssSelector(".tm-article-title__link")).getCssValue("clientHeight");
