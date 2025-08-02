a628a123975de08a7a64f1bee142e81331af6619
Andrey_O <a_reshkin@inbox.ru>
2025-08-02 23:27:06 +0300

    Update Project: - [Properties]

--- src/main/java/ru/merion/aqa/lesson4/Properties.java
+++ src/main/java/ru/merion/aqa/lesson4/Properties.java
@@ -12,7 +12,7 @@ public class Properties {
         driver.get("https://habr.com/ru/feed/");
 
         String target1 = driver.findElement(By.cssSelector(".tm-article-title__link")).getAttribute("host");
-        String target2 = driver.findElement(By.cssSelector(".tm-article-title__link")).getDomProperty("currentCSSZoom");
+        String target2 = driver.findElement(By.cssSelector(".tm-article-title__link")).getDomProperty("host");
         String target3 = driver.findElement(By.cssSelector(".tm-article-title__link")).getDomAttribute("currentCSSZoom");
         String target4 = driver.findElement(By.cssSelector(".tm-article-title__link")).getCssValue("currentCSSZoom");
 
