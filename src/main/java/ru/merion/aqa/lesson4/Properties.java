4c7f45afd51206b9f0d377a161219a56afed5377
Andrey_O <a_reshkin@inbox.ru>
2025-08-02 23:25:52 +0300

    Update Project: - [Properties]

--- src/main/java/ru/merion/aqa/lesson4/Properties.java
+++ src/main/java/ru/merion/aqa/lesson4/Properties.java
@@ -9,10 +9,10 @@ public class Properties {
     public static void main(String[] args) {
 
         WebDriver driver = WebDriverFactory.create("chrome");
-        driver.get("https://habr.com/ru/feed");
+        driver.get("https://habr.com/ru/feed/");
 
-        String target1 = driver.findElement(By.cssSelector(".tm-article-title__link")).getAttribute("contentEditable");
-        String target2 = driver.findElement(By.cssSelector(".tm-article-title__link")).getDomProperty("contentEditable");
+        String target1 = driver.findElement(By.cssSelector(".tm-article-title__link")).getAttribute("currentCSSZoom");
+        String target2 = driver.findElement(By.cssSelector(".tm-article-title__link")).getDomProperty("currentCSSZoom");
         String target3 = driver.findElement(By.cssSelector(".tm-article-title__link")).getDomAttribute("contentEditable");
         String target4 = driver.findElement(By.cssSelector(".tm-article-title__link")).getCssValue("contentEditable");
 
