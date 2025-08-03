e3a26cc3232495d25f8bef24a5a95649a56e1b3c
Andrey_O <a_reshkin@inbox.ru>
2025-08-03 15:55:07 +0300

    Update Project: - [Properties]

--- src/main/java/ru/merion/aqa/lesson4/Properties.java
+++ src/main/java/ru/merion/aqa/lesson4/Properties.java
@@ -13,7 +13,7 @@ public class Properties {
 
         String target1 = driver.findElement(By.cssSelector(".tm-article-title__link")).getAttribute("inert");
         String target2 = driver.findElement(By.cssSelector(".tm-article-title__link")).getDomProperty("inert");
-        String target3 = driver.findElement(By.cssSelector(".tm-article-title__link")).getDomAttribute("localName");
+        String target3 = driver.findElement(By.cssSelector(".tm-article-title__link")).getDomAttribute("inert");
         String target4 = driver.findElement(By.cssSelector(".tm-article-title__link")).getCssValue("localName");
 
 
