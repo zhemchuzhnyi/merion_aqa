16b869ac410f37a99df1b6fb62ed9df20f045b34
Andrey_O <a_reshkin@inbox.ru>
2025-08-03 15:57:06 +0300

    Update Project: - [Properties]

--- src/main/java/ru/merion/aqa/lesson4/Properties.java
+++ src/main/java/ru/merion/aqa/lesson4/Properties.java
@@ -11,7 +11,7 @@ public class Properties {
         WebDriver driver = WebDriverFactory.create("chrome");
         driver.get("https://habr.com/ru/feed/");
 
-        String target1 = driver.findElement(By.cssSelector(".tm-article-title__link")).getAttribute("namespaceURI");
+        String target1 = driver.findElement(By.cssSelector(".tm-article-title__link")).getAttribute("offsetLeft");
         String target2 = driver.findElement(By.cssSelector(".tm-article-title__link")).getDomProperty("namespaceURI");
         String target3 = driver.findElement(By.cssSelector(".tm-article-title__link")).getDomAttribute("namespaceURI");
         String target4 = driver.findElement(By.cssSelector(".tm-article-title__link")).getCssValue("namespaceURI");
