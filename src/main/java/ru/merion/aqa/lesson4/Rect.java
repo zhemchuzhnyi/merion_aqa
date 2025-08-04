4659f30cb36bc153dbfd0e8deda1367bbe857df7
Andrey_O <a_reshkin@inbox.ru>
2025-08-04 20:19:55 +0300

    Update Project: - [Rect]

--- src/main/java/ru/merion/aqa/lesson4/Rect.java
+++ src/main/java/ru/merion/aqa/lesson4/Rect.java
@@ -1,6 +1,8 @@
 package ru.merion.aqa.lesson4;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import ru.merion.aqa.WebDriverFactory;
 
 public class Rect {
@@ -8,8 +10,19 @@ public class Rect {
     public static void main(String[] args) {
 
         WebDriver driver = WebDriverFactory.create("chrome");
-
         driver.get("https://habr.com/ru/feed/");
+
+        WebElement el = driver.findElement(By.cssSelector(".tm-article-title__link"));
+
+        el.getRect().getDimension();
+        el.getRect().getWidth();
+        el.getRect().getHeight();
+        el.getLocation().getX();
+        el.getSize().getWidth();
+
+        driver.close();
+
+
 
     }
 }
