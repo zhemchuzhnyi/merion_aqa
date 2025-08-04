42a877f35331af74105076a0067d9269ffcfe3ac
Andrey_O <a_reshkin@inbox.ru>
2025-08-04 20:47:58 +0300

    Update Project: - [JSRemove Banner]

--- src/main/java/ru/merion/aqa/lesson4/JSCExecution.java
+++ src/main/java/ru/merion/aqa/lesson4/JSCExecution.java
@@ -1,5 +1,7 @@
 package ru.merion.aqa.lesson4;
 
+import org.openqa.selenium.JavascriptException;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import ru.merion.aqa.WebDriverFactory;
@@ -8,8 +10,12 @@ public class JSCExecution {
 
     public static void main(String[] args) {
 
+        String jsRemoveBanner = "document.querySelector(\".tabs-content\").remove()";
+
         WebDriver driver = WebDriverFactory.create("chrome");
         driver.get("https://mail.ru/");
+
+        ((JavascriptExecutor)driver).executeScript(jsRemoveBanner);
 
 
 
