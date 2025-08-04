0b22f297a3c48089c3823e470f4fc302a8db7a94
Andrey_O <a_reshkin@inbox.ru>
2025-08-04 20:11:07 +0300

    Update Project: - [Properties]

--- src/main/java/ru/merion/aqa/lesson4/Properties.java
+++ src/main/java/ru/merion/aqa/lesson4/Properties.java
@@ -16,6 +16,9 @@ public class Properties {
         String target3 = driver.findElement(By.cssSelector(".tm-article-title__link")).getDomAttribute("contentEditable");
         String target4 = driver.findElement(By.cssSelector(".tm-article-title__link")).getCssValue("contentEditable");
 
+        String innerText = driver.findElement(By.cssSelector(".tm-article-title__link")).getCssValue("innerText");
+        System.out.println(innerText);
+
 
 
         driver.quit();
