7847ea012676ca5215da6dacdd9c55ffec31b0df
Andrey_O <a_reshkin@inbox.ru>
2025-08-05 23:17:49 +0300

    Update Project: - [JSRemove Banner]

--- src/main/java/ru/merion/aqa/lesson4/Properties.java
+++ src/main/java/ru/merion/aqa/lesson4/Properties.java
@@ -14,7 +14,7 @@ public class Properties {
         String target1 = driver.findElement(By.cssSelector(".tm-article-title__link")).getAttribute("attributes");
         String target2 = driver.findElement(By.cssSelector(".tm-article-title__link")).getDomProperty("attributes");
         String target3 = driver.findElement(By.cssSelector(".tm-article-title__link")).getDomAttribute("attributes");
-        String target4 = driver.findElement(By.cssSelector(".tm-article-title__link")).getCssValue("attributes");
+        String target4 = driver.findElement(By.cssSelector(".tm-article-title__link")).getCssValue("autocorrect");
 
         String innerText = driver.findElement(By.cssSelector(".tm-article-title__link")).getAttribute("innerText"); // возвращает любой найденный текст
         System.out.println(innerText);
