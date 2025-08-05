87330109bc56c244976776d17bd391ef6040d119
Andrey_O <a_reshkin@inbox.ru>
2025-08-05 23:18:41 +0300

    Update Project: - [JSRemove Banner]

--- src/main/java/ru/merion/aqa/lesson4/Properties.java
+++ src/main/java/ru/merion/aqa/lesson4/Properties.java
@@ -12,7 +12,7 @@ public class Properties {
         driver.get("https://habr.com/ru/feed/");
 
         String target1 = driver.findElement(By.cssSelector(".tm-article-title__link")).getAttribute("clientHeight");
-        String target2 = driver.findElement(By.cssSelector(".tm-article-title__link")).getDomProperty("autocorrect");
+        String target2 = driver.findElement(By.cssSelector(".tm-article-title__link")).getDomProperty("clientHeight");
         String target3 = driver.findElement(By.cssSelector(".tm-article-title__link")).getDomAttribute("autocorrect");
         String target4 = driver.findElement(By.cssSelector(".tm-article-title__link")).getCssValue("autocorrect");
 
