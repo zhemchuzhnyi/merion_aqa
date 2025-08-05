d49f124d71ea56512e6ebd987844d304e667deed
Andrey_O <a_reshkin@inbox.ru>
2025-08-05 23:17:10 +0300

    Update Project: - [JSRemove Banner]

--- src/main/java/ru/merion/aqa/lesson4/Properties.java
+++ src/main/java/ru/merion/aqa/lesson4/Properties.java
@@ -12,7 +12,7 @@ public class Properties {
         driver.get("https://habr.com/ru/feed/");
 
         String target1 = driver.findElement(By.cssSelector(".tm-article-title__link")).getAttribute("attributes");
-        String target2 = driver.findElement(By.cssSelector(".tm-article-title__link")).getDomProperty("contentEditable");
+        String target2 = driver.findElement(By.cssSelector(".tm-article-title__link")).getDomProperty("attributes");
         String target3 = driver.findElement(By.cssSelector(".tm-article-title__link")).getDomAttribute("contentEditable");
         String target4 = driver.findElement(By.cssSelector(".tm-article-title__link")).getCssValue("contentEditable");
 
