6a9176421de53c668cbec3642aa85c5ed97aeb7e
Andrey_O <a_reshkin@inbox.ru>
2025-08-06 23:21:25 +0300

    Update Project: - [JSRemove Banner]

--- src/main/java/ru/merion/aqa/lesson4/Properties.java
+++ src/main/java/ru/merion/aqa/lesson4/Properties.java
@@ -11,7 +11,7 @@ public class Properties {
         WebDriver driver = WebDriverFactory.create("chrome");
         driver.get("https://habr.com/ru/feed/");
 
-        String target1 = driver.findElement(By.cssSelector(".tm-article-title__link")).getAttribute("draggable");
+        String target1 = driver.findElement(By.cssSelector(".tm-article-title__link")).getAttribute("clientWidth");
         String target2 = driver.findElement(By.cssSelector(".tm-article-title__link")).getDomProperty("clientWidth");
         String target3 = driver.findElement(By.cssSelector(".tm-article-title__link")).getDomAttribute("clientHeight");
         String target4 = driver.findElement(By.cssSelector(".tm-article-title__link")).getCssValue("clientWidth");
