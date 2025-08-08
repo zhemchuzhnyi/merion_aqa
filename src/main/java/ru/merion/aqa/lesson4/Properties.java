66d38dcf639d031f4f3381f1e9c9092d6073abaf
Andrey_O <a_reshkin@inbox.ru>
2025-08-08 17:14:19 +0300

    Update Project: - [Refactor]

--- src/main/java/ru/merion/aqa/lesson4/Properties.java
+++ src/main/java/ru/merion/aqa/lesson4/Properties.java
@@ -12,7 +12,7 @@ public class Properties {
         driver.get("https://habr.com/ru/feed/");
 
         String target1 = driver.findElement(By.cssSelector(".tm-article-title__link")).getAttribute("scrollWidth");
-        String target2 = driver.findElement(By.cssSelector(".tm-article-title__link")).getDomProperty("offsetTop");
+        String target2 = driver.findElement(By.cssSelector(".tm-article-title__link")).getDomProperty("scrollWidth");
         String target3 = driver.findElement(By.cssSelector(".tm-article-title__link")).getDomAttribute("offsetTop");
         String target4 = driver.findElement(By.cssSelector(".tm-article-title__link")).getCssValue("offsetTop");
 
