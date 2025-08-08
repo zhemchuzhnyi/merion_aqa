99724c26c33d7554ef9b4fd1ce6b06122effa1bd
Andrey_O <a_reshkin@inbox.ru>
2025-08-08 17:17:18 +0300

    Update Project: - [Refactor]

--- src/main/java/ru/merion/aqa/lesson4/Properties.java
+++ src/main/java/ru/merion/aqa/lesson4/Properties.java
@@ -14,7 +14,7 @@ public class Properties {
         String target1 = driver.findElement(By.cssSelector(".tm-article-title__link")).getAttribute("scrollWidth");
         String target2 = driver.findElement(By.cssSelector(".tm-article-title__link")).getDomProperty("scrollWidth");
         String target3 = driver.findElement(By.cssSelector(".tm-article-title__link")).getDomAttribute("scrollWidth");
-        String target4 = driver.findElement(By.cssSelector(".tm-article-title__link")).getCssValue("offsetTop");
+        String target4 = driver.findElement(By.cssSelector(".tm-article-title__link")).getCssValue("scrollWidth");
 
         String innerText = driver.findElement(By.cssSelector(".tm-article-title__link")).getAttribute("innerText"); // возвращает любой найденный текст
         System.out.println(innerText);
