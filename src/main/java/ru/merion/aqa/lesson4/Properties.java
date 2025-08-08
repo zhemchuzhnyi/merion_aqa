78bdbc1993af342ba12f7e0c77b911af802915a2
Andrey_O <a_reshkin@inbox.ru>
2025-08-08 17:10:23 +0300

    Update Project: - [Refactor]

--- src/main/java/ru/merion/aqa/lesson4/Properties.java
+++ src/main/java/ru/merion/aqa/lesson4/Properties.java
@@ -13,7 +13,7 @@ public class Properties {
 
         String target1 = driver.findElement(By.cssSelector(".tm-article-title__link")).getAttribute("offsetTop");
         String target2 = driver.findElement(By.cssSelector(".tm-article-title__link")).getDomProperty("offsetTop");
-        String target3 = driver.findElement(By.cssSelector(".tm-article-title__link")).getDomAttribute("contentEditable");
+        String target3 = driver.findElement(By.cssSelector(".tm-article-title__link")).getDomAttribute("offsetTop");
         String target4 = driver.findElement(By.cssSelector(".tm-article-title__link")).getCssValue("contentEditable");
 
         String innerText = driver.findElement(By.cssSelector(".tm-article-title__link")).getAttribute("innerText"); // возвращает любой найденный текст
