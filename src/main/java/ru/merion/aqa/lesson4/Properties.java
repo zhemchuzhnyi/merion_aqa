913c1b3026354586a390443772a6145e2804db02
Andrey_O <a_reshkin@inbox.ru>
2025-08-09 21:58:01 +0300

    Update Project: - [Refactor]

--- src/main/java/ru/merion/aqa/lesson4/Properties.java
+++ src/main/java/ru/merion/aqa/lesson4/Properties.java
@@ -13,7 +13,7 @@ public class Properties {
 
         String target1 = driver.findElement(By.cssSelector(".tm-article-title__link")).getAttribute("spellcheck");
         String target2 = driver.findElement(By.cssSelector(".tm-article-title__link")).getDomProperty("spellcheck");
-        String target3 = driver.findElement(By.cssSelector(".tm-article-title__link")).getDomAttribute("scrollWidth");
+        String target3 = driver.findElement(By.cssSelector(".tm-article-title__link")).getDomAttribute("spellcheck");
         String target4 = driver.findElement(By.cssSelector(".tm-article-title__link")).getCssValue("scrollWidth");
 
         String innerText = driver.findElement(By.cssSelector(".tm-article-title__link")).getAttribute("innerText"); // возвращает любой найденный текст
