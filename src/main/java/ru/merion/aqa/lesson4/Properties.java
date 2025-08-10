0c39a483d7747569bc40901f3572de46eefb09e3
Andrey_O <a_reshkin@inbox.ru>
2025-08-10 23:32:56 +0300

    Update Project: - [Refactor]

--- src/main/java/ru/merion/aqa/lesson4/Properties.java
+++ src/main/java/ru/merion/aqa/lesson4/Properties.java
@@ -14,7 +14,7 @@ public class Properties {
         String target1 = driver.findElement(By.cssSelector(".tm-article-title__link")).getAttribute("translate");
         String target2 = driver.findElement(By.cssSelector(".tm-article-title__link")).getDomProperty("translate");
         String target3 = driver.findElement(By.cssSelector(".tm-article-title__link")).getDomAttribute("translate");
-        String target4 = driver.findElement(By.cssSelector(".tm-article-title__link")).getCssValue("translate");
+        String target4 = driver.findElement(By.cssSelector(".tm-article-title__link")).getCssValue("spellcheck");
 
         String innerText = driver.findElement(By.cssSelector(".tm-article-title__link")).getAttribute("innerText"); // возвращает любой найденный текст
         System.out.println(innerText);
