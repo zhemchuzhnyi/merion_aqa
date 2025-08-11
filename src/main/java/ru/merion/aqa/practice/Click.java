6d30da0cc5540f682a5067b260c449a44722a0f8
Andrey_O <a_reshkin@inbox.ru>
2025-08-11 14:52:41 +0300

    Update Project: - [Practice - Click]

--- src/main/java/ru/merion/aqa/practice/Click.java
+++ src/main/java/ru/merion/aqa/practice/Click.java
@@ -1,14 +1,34 @@
 package ru.merion.aqa.practice;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import ru.merion.aqa.WebDriverFactory;
+
+import java.util.List;
 
 public class Click {
 
     public static void main(String[] args) {
 
         WebDriver driver = WebDriverFactory.create("chrome");
-        driver.get("http://the-internet.herokuapp.com/add_remove_elements/");
+        driver.get("https://the-internet.herokuapp.com/add_remove_elements/");
+        WebElement element = driver.findElement(By.tagName("button"));
+        element.click();
+        element.click();
+        element.click();
+        element.click();
+        element.click();
+
+        List<WebElement> deleteButtons = driver.findElements(By.className("added-manually"));
+
+
+        System.out.println("Количество кнопок Delete " + deleteButtons.size());
+
+
+
+        driver.quit();
+
 
     }
 }
