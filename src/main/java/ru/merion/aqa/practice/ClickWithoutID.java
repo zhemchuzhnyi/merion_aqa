9fd4aa6aaeb11d8346b872f25be42732a5353dca
Andrey_O <a_reshkin@inbox.ru>
2025-08-11 18:14:48 +0300

    Update Project: - [Practice - Click without ID]

--- src/main/java/ru/merion/aqa/practice/ClickWithoutID.java
+++ src/main/java/ru/merion/aqa/practice/ClickWithoutID.java
@@ -1,6 +1,8 @@
 package ru.merion.aqa.practice;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import ru.merion.aqa.WebDriverFactory;
 
 public class ClickWithoutID {
@@ -9,6 +11,10 @@ public class ClickWithoutID {
 
         WebDriver driver = WebDriverFactory.create("chrome");
         driver.get("https://uitestingplayground.com/dynamicid");
+        WebElement element = driver.findElement(By.id("08ed7121-b197-2334-149e-7e6f6f5ab439"));
+        element.click();
+
+        driver.quit();
 
     }
 }
