f12eebf395473c60536d429cf252dcb6ce486299
Andrey_O <a_reshkin@inbox.ru>
2025-08-11 22:39:34 +0300

    Update Project: - [Click with css button]

--- src/main/java/ru/merion/aqa/practiceDZ/Task3.java
+++ src/main/java/ru/merion/aqa/practiceDZ/Task3.java
@@ -2,6 +2,7 @@ package ru.merion.aqa.practiceDZ;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import ru.merion.aqa.WebDriverFactory;
 
 public class Task3 {
@@ -10,7 +11,9 @@ public class Task3 {
 
         WebDriver driver = WebDriverFactory.create("chrome");
         driver.get("http://uitestingplayground.com/classattr");
-        driver.findElement(By.cssSelector(".class3"));
+        WebElement element = driver.findElement(By.cssSelector(".class3"));
+        element.click();
+        driver.navigate().refresh();
 
         driver.quit();
     }
