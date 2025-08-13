9ca64990c180ae1b92651e026343ccd7baceb01d
Andrey_O <a_reshkin@inbox.ru>
2025-08-13 23:21:56 +0300

    Update Project: - [Input]

--- src/main/java/ru/merion/aqa/practiceDZ/Task5.java
+++ src/main/java/ru/merion/aqa/practiceDZ/Task5.java
@@ -1,5 +1,6 @@
 package ru.merion.aqa.practiceDZ;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import ru.merion.aqa.WebDriverFactory;
@@ -9,7 +10,7 @@ public class Task5 {
     public static void main(String[] args) {
         WebDriver driver = WebDriverFactory.create("chrome");
         driver.get("http://the-internet.herokuapp.com/inputs");
-        WebElement element = driver.
+        WebElement element = driver.findElement(By.cssSelector("."));
     }
 
 }
