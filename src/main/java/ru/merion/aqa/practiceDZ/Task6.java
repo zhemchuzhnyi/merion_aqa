eac846f6ed6c7fb404308177d761c45ed1b4571d
Andrey_O <a_reshkin@inbox.ru>
2025-08-14 22:58:12 +0300

    Update Project: - [Login]

--- src/main/java/ru/merion/aqa/practiceDZ/Task6.java
+++ src/main/java/ru/merion/aqa/practiceDZ/Task6.java
@@ -1,6 +1,7 @@
 package ru.merion.aqa.practiceDZ;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import ru.merion.aqa.WebDriverFactory;
 
 public class Task6 {
@@ -9,6 +10,7 @@ public class Task6 {
 
         WebDriver driver = WebDriverFactory.create("chrome");
         driver.get("http://the-internet.herokuapp.com/login");
+        WebElement element = driver.
 
 
 
