cdfba18421e353ce8f43c780783d14dcc108255f
Andrey_O <a_reshkin@inbox.ru>
2025-08-15 13:56:34 +0300

    Update Project: - [Modal Window]

--- src/main/java/ru/merion/aqa/practiceDZ/Task4.java
+++ src/main/java/ru/merion/aqa/practiceDZ/Task4.java
@@ -5,18 +5,20 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import ru.merion.aqa.WebDriverFactory;
 
+import java.util.List;
+
 public class Task4 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         WebDriver driver = WebDriverFactory.create("chrome");
         driver.get("http://the-internet.herokuapp.com/entry_ad");
-        WebElement element = driver.findElement(By.cssSelector("."));
 
+        WebElement element = driver.findElement(By.cssSelector(".modal-footer"));
         element.click();
+        List<WebElement> content = driver.findElements(By.cssSelector(".large-12.columns"));
 
-
-
+        System.out.println("Текст элемента content: " + content.size());
 
         driver.quit();
 
