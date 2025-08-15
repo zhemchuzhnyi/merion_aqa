dc785fc80aad34c3ed4960fb945bfa5c4db9bcd2
Andrey_O <a_reshkin@inbox.ru>
2025-08-15 13:58:06 +0300

    Update Project: - [Modal Window]

--- src/main/java/ru/merion/aqa/practiceDZ/Task4.java
+++ src/main/java/ru/merion/aqa/practiceDZ/Task4.java
@@ -3,6 +3,7 @@ package ru.merion.aqa.practiceDZ;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import ru.merion.aqa.WebDriverFactory;
 
 import java.util.List;
@@ -11,12 +12,11 @@ public class Task4 {
 
     public static void main(String[] args) throws InterruptedException {
 
-        WebDriver driver = WebDriverFactory.create("chrome");
+        WebDriver driver = new ChromeDriver();
         driver.get("http://the-internet.herokuapp.com/entry_ad");
         Thread.sleep(3000);
 
-        WebElement element = driver.findElement(By.cssSelector(".modal-footer"));
-        element.click();
+        driver.findElement(By.cssSelector(".modal-footer")).click();
         List<WebElement> content = driver.findElements(By.cssSelector(".large-12.columns"));
 
         System.out.println("Текст элемента content: " + content.size());
