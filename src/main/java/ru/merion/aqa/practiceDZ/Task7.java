977bb1b328b43e9c09d4819502e11f6133277a5d
Andrey_O <a_reshkin@inbox.ru>
2025-08-16 20:30:59 +0300

    Update Project: - [refactor]

--- src/main/java/ru/merion/aqa/practiceDZ/Task7.java
+++ src/main/java/ru/merion/aqa/practiceDZ/Task7.java
@@ -21,9 +21,15 @@ https://wiki.merionet.ru
 
 package ru.merion.aqa.practiceDZ;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Task7 {
 
     public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+
 
     }
 
