0daf53e0ede61987877ddb83026aed344eaa3114
Andrey_O <a_reshkin@inbox.ru>
2025-08-18 22:00:35 +0300

    Update Project: - [input]

--- src/main/java/ru/merion/aqa/practiceDZ/Task5.java
+++ src/main/java/ru/merion/aqa/practiceDZ/Task5.java
@@ -23,7 +23,9 @@ public class Task5 {
     public static void main(String[] args) {
         WebDriver driver = WebDriverFactory.create("chrome");
         driver.get("http://the-internet.herokuapp.com/inputs");
-        WebElement element = driver.findElement(By.cssSelector("input"));
+        driver.findElement(By.cssSelector("input")).click();
+
+
 
         driver.quit();
     }
