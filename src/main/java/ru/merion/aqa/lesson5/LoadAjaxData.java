c2f30f6b4f097f3b7621cb59fc0daa69f8abda0d
Andrey_O <a_reshkin@inbox.ru>
2025-08-21 21:45:01 +0300

    Update Project: - [Loading]

--- src/main/java/ru/merion/aqa/lesson5/LoadAjaxData.java
+++ src/main/java/ru/merion/aqa/lesson5/LoadAjaxData.java
@@ -1,6 +1,8 @@
 package ru.merion.aqa.lesson5;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import ru.merion.aqa.WebDriverFactory;
 
 public class LoadAjaxData {
@@ -8,6 +10,8 @@ public class LoadAjaxData {
     public static void main(String[] args) {
 
         WebDriver driver = WebDriverFactory.create("chrome");
+        driver.get("http://uitestingplayground.com/ajax");
+        WebElement button = driver.findElement(By.cssSelector("#ajax-button"));
 
     }
 }
