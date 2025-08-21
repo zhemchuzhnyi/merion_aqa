bede499abf16fcae09bfa30c4e7a2e9e0e3f1daf
Andrey_O <a_reshkin@inbox.ru>
2025-08-21 21:56:06 +0300

    Update Project: - [Loading]

--- src/main/java/ru/merion/aqa/lesson5/LoadAjaxData.java
+++ src/main/java/ru/merion/aqa/lesson5/LoadAjaxData.java
@@ -10,11 +10,13 @@ import ru.merion.aqa.WebDriverFactory;
 
 public class LoadAjaxData {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         WebDriver driver = WebDriverFactory.create("chrome");
 
         driver.get("http://uitestingplayground.com/ajax");
         driver.findElement(By.cssSelector("#ajaxButton")).click(); // - нажмет, но контент не получит потому что долгая загрузка после клика
+        Thread.sleep(16 * 1000);
+
         driver.findElement(By.cssSelector("#content p")).getText();
         String content = driver.findElement(By.cssSelector("#content p")).getText();
 
