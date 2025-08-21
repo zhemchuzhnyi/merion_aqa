7d8aa752f29edd1cc8a07ef1a02e9f3c4bae96d6
Andrey_O <a_reshkin@inbox.ru>
2025-08-21 21:42:47 +0300

    Update Project: - [Loading]

---
+++ src/main/java/ru/merion/aqa/lesson5/LoadAjaxData.java
@@ -0,0 +1,13 @@
+package ru.merion.aqa.lesson5;
+
+import org.openqa.selenium.WebDriver;
+import ru.merion.aqa.WebDriverFactory;
+
+public class LoadAjaxData {
+
+    public static void main(String[] args) {
+
+        WebDriver driver = WebDriverFactory.create("chrome");
+
+    }
+}
