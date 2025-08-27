57c9d9952d8996bc7a56db1c7cccaa2a7d959c4c
Andrey_O <a_reshkin@inbox.ru>
2025-08-27 23:07:46 +0300

    Update Project: - [Progressbar]

--- src/main/java/ru/merion/aqa/lesson5/Progressbar.java
+++ src/main/java/ru/merion/aqa/lesson5/Progressbar.java
@@ -16,15 +16,33 @@ public class Progressbar {
 
         driver.findElement(By.cssSelector("#startButton")).click();
 
-        for (int i = 0; i < 1000; i++){
-            String value = driver.findElement(By.cssSelector("#progressBar")).getAttribute("aria-valuenow");
-            System.out.println(value);
-            Thread.sleep(100);
-            if (Integer.parseInt(value) >= 75 ){
-                driver.findElement(By.cssSelector("#stopButton")).click();
-                break;
-            }
+        /*
+        Можно решить задачу таким образом
+        Нужно на прогрессбаре получить 75%
+         */
+
+  //      for (int i = 0; i < 1000; i++){
+  //          String value = driver.findElement(By.cssSelector("#progressBar")).getAttribute("aria-valuenow");
+  //          System.out.println(value);
+  //          Thread.sleep(100);
+  //          if (Integer.parseInt(value) >= 75 ){
+  //              driver.findElement(By.cssSelector("#stopButton")).click();
+  //              break;
+  //          }
+  //      }
+        /*
+        Либо решить таким способом, но это тоже такое себе решение
+         */
+
+    while (true) {
+        String value = driver.findElement(By.cssSelector("#progressBar")).getAttribute("aria-valuenow");
+        System.out.println(value);
+        Thread.sleep(100);
+        if (Integer.parseInt(value) >= 75 ) {
+            driver.findElement(By.cssSelector("#stopButton")).click();
+            break;
         }
+    }
 
         System.out.println("Finished");
 
