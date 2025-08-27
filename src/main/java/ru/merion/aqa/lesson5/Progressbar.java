c71c07ffaecc1c5cc70c1d37d8d3c4c92799eb0e
Andrey_O <a_reshkin@inbox.ru>
2025-08-27 22:35:04 +0300

    Update Project: - [Progressbar]

--- src/main/java/ru/merion/aqa/lesson5/Progressbar.java
+++ src/main/java/ru/merion/aqa/lesson5/Progressbar.java
@@ -1,5 +1,6 @@
 package ru.merion.aqa.lesson5;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import ru.merion.aqa.WebDriverFactory;
 
@@ -8,6 +9,10 @@ public class Progressbar {
 
         WebDriver driver = WebDriverFactory.create("chrome");
         driver.get("http://uitestingplayground.com/progressbar");
+
+        driver.findElement(By.cssSelector("#startButton")).click();
+
+        driver.findElement(By.cssSelector("#stopButton")).click();
 
     }
 
