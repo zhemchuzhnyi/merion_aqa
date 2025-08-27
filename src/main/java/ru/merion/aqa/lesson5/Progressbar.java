09c213d199147265e0b11c2e450b56a769538d85
Andrey_O <a_reshkin@inbox.ru>
2025-08-27 22:58:24 +0300

    Update Project: - [Progressbar]

--- src/main/java/ru/merion/aqa/lesson5/Progressbar.java
+++ src/main/java/ru/merion/aqa/lesson5/Progressbar.java
@@ -1,3 +1,7 @@
+/*
+Прогресс бар
+ */
+
 package ru.merion.aqa.lesson5;
 
 import org.openqa.selenium.By;
@@ -5,7 +9,7 @@ import org.openqa.selenium.WebDriver;
 import ru.merion.aqa.WebDriverFactory;
 
 public class Progressbar {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         WebDriver driver = WebDriverFactory.create("chrome");
         driver.get("http://uitestingplayground.com/progressbar");
@@ -13,12 +17,16 @@ public class Progressbar {
         driver.findElement(By.cssSelector("#startButton")).click();
 
         for (int i = 0; i < 1000; i++){
-            String value = driver.findElement(By.cssSelector("#progressBar")).getAttribute("aria-valuemax");
+            String value = driver.findElement(By.cssSelector("#progressBar")).getAttribute("aria-valuenow");
             System.out.println(value);
-            if (Integer.parseInt(value) >= 75){
+            Thread.sleep(100);
+            if (Integer.parseInt(value) >= 75 ){
                 driver.findElement(By.cssSelector("#stopButton")).click();
+                break;
             }
         }
+
+        System.out.println("Finished");
 
 
         driver.quit();
