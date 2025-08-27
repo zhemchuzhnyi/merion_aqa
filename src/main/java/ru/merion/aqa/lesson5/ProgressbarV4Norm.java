c9b49c6788ee863b9aff6f891b2ff70c9b83170d
Andrey_O <a_reshkin@inbox.ru>
2025-08-28 00:00:38 +0300

    Update Project: - [Progressbar - явное ожидание - bestpractics]

---
+++ src/main/java/ru/merion/aqa/lesson5/ProgressbarV4Norm.java
@@ -0,0 +1,32 @@
+/*
+Прогресс бар - правильно решение задачи
+ */
+package ru.merion.aqa.lesson5;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import ru.merion.aqa.WebDriverFactory;
+
+import java.time.Duration;
+
+public class ProgressbarV4Norm {
+
+    public static void main(String[] args) {
+        WebDriver driver = WebDriverFactory.create("chrome");
+        driver.get("http://uitestingplayground.com/progressbar");
+
+        driver.findElement(By.cssSelector("#startButton")).click();
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(60));
+        wait.until(ExpectedConditions.textToBe(By.cssSelector("#progressBar"), "75%"));
+
+        driver.findElement(By.cssSelector("#stopButton")).click();
+        System.out.println("Finished");
+
+        driver.quit();
+
+    }
+}
