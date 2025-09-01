27a27caadf84bb7f4a87be16649fb700c20a47a9
Andrey_O <a_reshkin@inbox.ru>
2025-09-02 00:03:00 +0300

    Update Project: - []

--- src/main/java/ru/merion/aqa/lesson5/ECDemo.java
+++ src/main/java/ru/merion/aqa/lesson5/ECDemo.java
@@ -1,6 +1,14 @@
+/*
+EC - Expected Conditons
+ */
+
 package ru.merion.aqa.lesson5;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import ru.merion.aqa.WebDriverFactory;
 
@@ -11,7 +19,10 @@ public class ECDemo {
     public static void main(String[] args) {
         WebDriver driver = WebDriverFactory.create("chrome");
 
+        WebElement element = driver.findElement(By.cssSelector(""));
+
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+        wait.until(ExpectedConditions.stalenessOf(element));
 
     }
 }
