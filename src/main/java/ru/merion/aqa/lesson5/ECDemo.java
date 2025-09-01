1fe6f4390daba8774ce261b44b4aaafbebacdd52
Andrey_O <a_reshkin@inbox.ru>
2025-09-02 00:00:58 +0300

    Update Project: - []

--- src/main/java/ru/merion/aqa/lesson5/ECDemo.java
+++ src/main/java/ru/merion/aqa/lesson5/ECDemo.java
@@ -1,5 +1,6 @@
 package ru.merion.aqa.lesson5;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import ru.merion.aqa.WebDriverFactory;
 
@@ -8,7 +9,7 @@ import java.time.Duration;
 public class ECDemo {
 
     public static void main(String[] args) {
-        WebDriverFactory factory = new WebDriverFactory.create();
+        WebDriver driver = WebDriverFactory.create("chrome");
 
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
 
