8bc2ac945f70970feb1ec734f4a982b517ee2569
Andrey_O <a_reshkin@inbox.ru>
2025-09-02 00:20:32 +0300

    Update Project: - [Expected Conditions]

--- src/main/java/ru/merion/aqa/lesson5/ECDemo.java
+++ src/main/java/ru/merion/aqa/lesson5/ECDemo.java
@@ -21,14 +21,21 @@ public class ECDemo {
         WebDriver driver = WebDriverFactory.create("chrome");
 
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
-        //дождаться исчезновение (!) элемента
+
+        // дождаться исчезновение (!) элемента
         wait.until(ExpectedConditions.stalenessOf(driver.findElement(By.cssSelector(""))));
 
-        //дождаться точного количества элементов на странице
+        // дождаться точного количества элементов на странице
         List<WebElement> elements = wait.until(ExpectedConditions.numberOfElementsToBe(By.cssSelector(""), 7));
 
-        //дождаться видимости элемента
+        // дождаться видимости элемента
         wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.cssSelector("")))).click();
+
+        // дождаться появления alert/confirm/prompt и перейти в него
+        wait.until(ExpectedConditions.alertIsPresent());
+
+        //
+
 
 
 
