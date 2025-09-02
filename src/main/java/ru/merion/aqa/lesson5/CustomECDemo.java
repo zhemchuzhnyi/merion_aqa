1a6543a8ec55e0653b9710cb78e894d3b6671c99
Andrey_O <a_reshkin@inbox.ru>
2025-09-02 21:13:29 +0300

    Update Project: - [Custom Expected Conditions]

--- src/main/java/ru/merion/aqa/lesson5/CustomECDemo.java
+++ src/main/java/ru/merion/aqa/lesson5/CustomECDemo.java
@@ -3,11 +3,17 @@ package ru.merion.aqa.lesson5;
 import org.openqa.selenium.WebDriver;
 import ru.merion.aqa.WebDriverFactory;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class CustomECDemo {
 
     public static void main(String[] args) {
+        Path relativePath = Path.of("/Users/a0000/IdeaProjects/merion_aqa/src/main/resources/dummu_pages/old_vk.html");
+        String filePath = Paths.get("").toAbsolutePath().resolve(relativePath).toString();
 
         WebDriver driver = WebDriverFactory.create("chrome");
+        driver.get("file:// + filePath");
 
     }
 }
