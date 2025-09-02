2b3af31c300779b615bfed872fd4f3fdfd64c05b
Andrey_O <a_reshkin@inbox.ru>
2025-09-02 21:07:46 +0300

    Update Project: - [Expected Conditions]

---
+++ src/main/java/ru/merion/aqa/lesson5/CustomECDemo.java
@@ -0,0 +1,13 @@
+package ru.merion.aqa.lesson5;
+
+import org.openqa.selenium.WebDriver;
+import ru.merion.aqa.WebDriverFactory;
+
+public class CustomECDemo {
+
+    public static void main(String[] args) {
+
+        WebDriver driver = WebDriverFactory.create("chrome");
+
+    }
+}
