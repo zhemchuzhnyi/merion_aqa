d806c5af65dffb39d836e27ab06db7aff409a991
Andrey_O <a_reshkin@inbox.ru>
2025-09-03 20:25:28 +0300

    Update Project: - [Practice DZ_2 Task_1]

---
+++ src/main/java/ru/merion/aqa/practiceDZ2/Task1.java
@@ -0,0 +1,21 @@
+/*
+Нажатие на кнопку
+ */
+
+package ru.merion.aqa.practiceDZ2;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import ru.merion.aqa.WebDriverFactory;
+
+public class Task1 {
+
+    public static void main(String[] args) {
+        WebDriver driver = WebDriverFactory.create("chrome");
+        driver.get("http://uitestingplayground.com/ajax");
+        driver.findElement(By.cssSelector("#ajaxButton")).click();
+
+
+    }
+}
