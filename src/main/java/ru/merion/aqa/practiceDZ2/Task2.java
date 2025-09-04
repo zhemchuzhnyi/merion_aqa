5358422ed2c93bb312691d6f46a0f1fc4661dbb2
Andrey_O <a_reshkin@inbox.ru>
2025-09-04 22:02:16 +0300

    Update Project: - [PracticeDZ_2 Task_2]

--- src/main/java/ru/merion/aqa/practiceDZ2/Task2.java
+++ src/main/java/ru/merion/aqa/practiceDZ2/Task2.java
@@ -1,5 +1,16 @@
+/*
+Переименовать кнопку
+Напишите скрипт. Шаги:
+
+Перейти на сайт http://uitestingplayground.com/textinput
+Указать в поле ввода текст "Merion"
+Нажать на синюю кнопку
+Получить текст кнопки и вывести в консоль (Merion)
+ */
+
 package ru.merion.aqa.practiceDZ2;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import ru.merion.aqa.WebDriverFactory;
 
@@ -7,5 +18,7 @@ public class Task2 {
 
     public static void main(String[] args) {
         WebDriver driver = WebDriverFactory.create("chrome");
+        driver.get("http://uitestingplayground.com/textinput");
+        driver.findElement(By.cssSelector("#updatingButton"));
     }
 }
