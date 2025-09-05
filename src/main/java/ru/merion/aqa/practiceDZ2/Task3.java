d30b438104634a03f31fee2cae6b9efe16c48174
Andrey_O <a_reshkin@inbox.ru>
2025-09-05 21:15:45 +0300

    Update Project: - [PracticeDZ_2 Task_3]

---
+++ src/main/java/ru/merion/aqa/practiceDZ2/Task3.java
@@ -0,0 +1,18 @@
+/*
+Напишите скрипт. Шаги:
+
+Перейти на сайт https://bonigarcia.dev/selenium-webdriver-java/loading-images.html
+Дождаться загрузки 3й картинки
+Получить значение атрибута
+src
+ у 3й картинки
+Вывести значение в консоль
+ */
+package ru.merion.aqa.practiceDZ2;
+
+public class Task3 {
+
+    public static void main(String[] args) {
+
+    }
+}
