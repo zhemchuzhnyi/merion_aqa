7eb5debfc7dc3c9f570ab23cb853deac82bb9587
Andrey_O <a_reshkin@inbox.ru>
2025-09-06 12:11:42 +0300

    Update Project: - [PracticeDZ_2 Task_3]

--- src/main/java/ru/merion/aqa/practiceDZ2/Task3.java
+++ src/main/java/ru/merion/aqa/practiceDZ2/Task3.java
@@ -21,13 +21,12 @@ import java.time.Duration;
 
 public class Task3 {
 
-    private static org.openqa.selenium.support.ui.WebDriverWait WebDriverWait;
-
     public static void main(String[] args) {
         WebDriver driver = WebDriverFactory.create("chrome");
         driver.get("https://bonigarcia.dev/selenium-webdriver-java/loading-images.html");
-        WebDriverWait = new WebDriverWait(driver, Duration.ofSeconds(15));
-        WebElement src = driver.findElement();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+        wait.until();
+        WebElement src = driver.findElement(By.id("src"));
         driver.findElement(By.cssSelector(""));
 
 
