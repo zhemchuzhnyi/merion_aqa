ab372c95a1980ef15c4722e3c66c7f3326d355f5
Andrey_O <a_reshkin@inbox.ru>
2025-09-07 22:42:33 +0300

    Update Project: - [PracticeDZ_2 Task_4]

--- src/main/java/ru/merion/aqa/practiceDZ2/Task4.java
+++ src/main/java/ru/merion/aqa/practiceDZ2/Task4.java
@@ -29,9 +29,13 @@ Phone
  */
 package ru.merion.aqa.practiceDZ2;
 
+import org.openqa.selenium.WebDriver;
+import ru.merion.aqa.WebDriverFactory;
+
 public class Task4 {
 
     public static void main(String[] args) {
+        WebDriver driver = WebDriverFactory.create("chrome");
 
     }
 }
