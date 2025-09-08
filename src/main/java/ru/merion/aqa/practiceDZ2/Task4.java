0e959fbbe80ba54fce5b1a9da99dc358e04e23d2
Andrey_O <a_reshkin@inbox.ru>
2025-09-08 22:40:18 +0300

    Update Project: - [PracticeDZ_2 Task_4]

--- src/main/java/ru/merion/aqa/practiceDZ2/Task4.java
+++ src/main/java/ru/merion/aqa/practiceDZ2/Task4.java
@@ -43,6 +43,7 @@ public class Task4 {
         driver.findElement(By.cssSelector("input[name='address']")).sendKeys("Ленина, 55-3");
         driver.findElement(By.cssSelector("input[name='city']")).sendKeys("Москва");
         driver.findElement(By.cssSelector("input[name='country']")).sendKeys("Россия");
+        driver.findElement(By.cssSelector("input[name='country']")).sendKeys();
 
 
         driver.quit();
