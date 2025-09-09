94a99e013371585cccb321a7615001b4c5b20e7e
Andrey_O <a_reshkin@inbox.ru>
2025-09-09 20:44:03 +0300

    Update Project: - [PracticeDZ_2 Task_5]

---
+++ src/main/java/ru/merion/aqa/practiceDZ2/Task5.java
@@ -0,0 +1,26 @@
+/*
+Скрипт на калькулятор
+Напишите скрипт. Шаги:
+
+Открыть страницу https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html
+В поле ввода по локатору
+#delay
+ ввести значение 45
+Нажать на кнопки
+7
++(плюс)
+8
+=
+Дождаться результата. Вывести его в консоль.
+ */
+package ru.merion.aqa.practiceDZ2;
+
+import org.openqa.selenium.WebDriver;
+import ru.merion.aqa.WebDriverFactory;
+
+public class Task5 {
+
+    public static void main(String[] args) {
+        WebDriver driver = WebDriverFactory.create("chrome");
+    }
+}
