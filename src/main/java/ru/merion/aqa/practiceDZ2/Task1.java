2fe3de495b7620715cd243a09af098b3357aa735
Andrey_O <a_reshkin@inbox.ru>
2025-09-11 22:08:32 +0300

    Update Project: - [Refactor Task_1]

--- src/main/java/ru/merion/aqa/practiceDZ2/Task1.java
+++ src/main/java/ru/merion/aqa/practiceDZ2/Task1.java
@@ -12,15 +12,9 @@ package ru.merion.aqa.practiceDZ2;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Wait;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import ru.merion.aqa.WebDriverFactory;
 
 import java.time.Duration;
-
-import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOf;
 
 public class Task1 {
 
