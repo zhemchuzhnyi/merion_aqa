9a26f719a7c01285dfea26607f1aae9c66a84686
Andrey_O <a_reshkin@inbox.ru>
2025-09-11 22:48:39 +0300

    Update Project: - [New Solution Task_5]

--- src/main/java/ru/merion/aqa/practiceDZ2/Task5NewSolution.java
+++ src/main/java/ru/merion/aqa/practiceDZ2/Task5NewSolution.java
@@ -1,6 +1,8 @@
 package ru.merion.aqa.practiceDZ2;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import ru.merion.aqa.WebDriverFactory;
 
 public class Task5NewSolution {
@@ -10,6 +12,11 @@ public class Task5NewSolution {
 
         WebDriver driver = WebDriverFactory.create("chrome");
         driver.get("https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html");
+
+        driver.findElement(By.cssSelector("#delay")).click();
+        driver.findElement(By.cssSelector("#delay")).sendKeys(String.valueOf(timeout));
+
+        WebElement keyboard = driver.findElement(By.cssSelector(".keys"));
 
     }
 }
