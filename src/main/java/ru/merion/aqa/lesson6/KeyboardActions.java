be4dee479e97cb13c734dc73c6b9257ab635821b
Andrey_O <a_reshkin@inbox.ru>
2025-09-16 22:00:56 +0300

    Update Project: - [Keyboard Actions]

--- src/main/java/ru/merion/aqa/lesson6/KeyboardActions.java
+++ src/main/java/ru/merion/aqa/lesson6/KeyboardActions.java
@@ -1,6 +1,8 @@
 package ru.merion.aqa.lesson6;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
 import ru.merion.aqa.WebDriverFactory;
 
 public class KeyboardActions {
@@ -8,5 +10,9 @@ public class KeyboardActions {
     public static void main(String[] args) {
         WebDriver driver = WebDriverFactory.create("chrome");
         driver.get("http://uitestingplayground.com/textinput");
+
+        Actions act = new Actions(driver);
+
+        By locator = By.cssSelector("#newButtonName");
     }
 }
