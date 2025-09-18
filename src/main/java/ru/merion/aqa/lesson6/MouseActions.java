39ddceca42780efac01c5a1bac20e2a090c76aa0
Andrey_O <a_reshkin@inbox.ru>
2025-09-18 22:19:42 +0300

    Update Project: - [Keyboard Actions]

---
+++ src/main/java/ru/merion/aqa/lesson6/MouseActions.java
@@ -0,0 +1,13 @@
+package ru.merion.aqa.lesson6;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class MouseActions {
+
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver
+
+    }
+}
