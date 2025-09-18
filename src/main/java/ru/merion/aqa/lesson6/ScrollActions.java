97d19b20f63dd7d0a3d22017463831fb66cca098
Andrey_O <a_reshkin@inbox.ru>
2025-09-19 01:36:35 +0300

    Update Project: - [Scroll Actions]

---
+++ src/main/java/ru/merion/aqa/lesson6/ScrollActions.java
@@ -0,0 +1,14 @@
+package ru.merion.aqa.lesson6;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class ScrollActions {
+
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+}
