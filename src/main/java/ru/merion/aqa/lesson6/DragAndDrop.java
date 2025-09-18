573bcc4d1b9ff6d64396ab805808f6a2e98c2fb4
Andrey_O <a_reshkin@inbox.ru>
2025-09-19 00:38:40 +0300

    Update Project: - [Mouse Actions]

---
+++ src/main/java/ru/merion/aqa/lesson6/DragAndDrop.java
@@ -0,0 +1,16 @@
+package ru.merion.aqa.lesson6;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class DragAndDrop {
+
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        driver.get("");
+    }
+}
