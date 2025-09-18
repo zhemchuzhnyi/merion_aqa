da93bfd385bff78b43eb0d4289944e59786e3579
Andrey_O <a_reshkin@inbox.ru>
2025-09-18 22:29:53 +0300

    Update Project: - [Keyboard Actions]

--- src/main/java/ru/merion/aqa/lesson6/MouseActions.java
+++ src/main/java/ru/merion/aqa/lesson6/MouseActions.java
@@ -1,6 +1,8 @@
 package ru.merion.aqa.lesson6;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class MouseActions {
@@ -8,6 +10,11 @@ public class MouseActions {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
         driver.get("https://jspaint.app/#local:f027a292cdf1");
+
+        WebElement canvas = driver.findElement(By.cssSelector("#main-canvas"));
+        canvas.click();
+
+        driver.quit();
 
 
 
