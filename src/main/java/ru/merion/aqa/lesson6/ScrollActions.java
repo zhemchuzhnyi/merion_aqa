fd5ddbca5069d9a6fd7abc29fae9d50e707e08b9
Andrey_O <a_reshkin@inbox.ru>
2025-09-22 22:40:29 +0300

    Update Project: - [Update]

--- src/main/java/ru/merion/aqa/lesson6/ScrollActions.java
+++ src/main/java/ru/merion/aqa/lesson6/ScrollActions.java
@@ -1,7 +1,10 @@
 package ru.merion.aqa.lesson6;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.interactions.WheelInput;
 
 import java.time.Duration;
 
@@ -10,6 +13,17 @@ public class ScrollActions {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        driver.get("");
+        driver.get("https://www.apple.com/iphone-17-pro/");
+
+        By iconLocator = By.cssSelector(".ac-gf-breadcrumbs-home-icon");
+
+        new Actions(driver)
+                .pause(1000L)
+                .scrollFromOrigin(WheelInput.ScrollOrigin.fromElement(driver.findElement(iconLocator)),0,200)
+                        .pause(1000L)
+                                .perform();
+
+        driver.quit();
+
     }
 }
