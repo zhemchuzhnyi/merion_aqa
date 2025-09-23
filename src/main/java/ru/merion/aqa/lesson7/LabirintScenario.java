34e2a33c374186730d4b1826a9c48f8a20830f64
Andrey_O <a_reshkin@inbox.ru>
2025-09-23 23:16:20 +0300

    Update Project: - [Lesson 7 Page Object]

--- src/main/java/ru/merion/aqa/lesson7/LabirintScenario.java
+++ src/main/java/ru/merion/aqa/lesson7/LabirintScenario.java
@@ -3,13 +3,21 @@ package ru.merion.aqa.lesson7;
 import org.openqa.selenium.Cookie;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class LabirintScenario {
 
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(4));
         driver.get("https://www.labirint.ru/");
         Cookie cookie = new Cookie("cookie_policy", "1");
         driver.manage().addCookie(cookie);
+        driver.navigate().refresh();
+        driver.get("https://www.labirint.ru/");
+
+
     }
 }
