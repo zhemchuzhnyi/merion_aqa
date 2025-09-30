83249432b91b7a7e75c88217fe9743851f66dc53
Andrey_O <a_reshkin@inbox.ru>
2025-09-30 23:55:49 +0300

    Update Project: - [Lesson 7 - Page Object]

--- src/main/java/ru/merion/aqa/lesson7/LabirintScenario.java
+++ src/main/java/ru/merion/aqa/lesson7/LabirintScenario.java
@@ -1,15 +1,11 @@
 package ru.merion.aqa.lesson7;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.Cookie;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import ru.merion.aqa.WebDriverFactory;
+import ru.merion.aqa.lesson7.page.CartPage;
 import ru.merion.aqa.lesson7.page.MainPage;
 import ru.merion.aqa.lesson7.page.ResultPage;
-
-import java.time.Duration;
-import java.util.List;
 
 
 public class LabirintScenario {
@@ -30,30 +26,12 @@ public class LabirintScenario {
         resultPage.addAllItemsToCart();
         resultPage.checkIconText();
 
-        openCart();
-        checkCartCounter();
-        checkCartPrice();
+        CartPage cartPage = new CartPage(driver);
+        cartPage.open();
+        cartPage.checkCartCounter();
+        cartPage.checkCartPrice();
 
         // Закрываем браузер и завершаем сессию WebDriver
         driver.close();
-    }
-
-    public static void openCart() {
-
-        // Кликаем по иконке корзины для перехода на страницу корзины
-        driver.findElement(cartIconLocator).click();
-    }
-    public static void checkCartCounter () {
-
-        // Находим и выводим счётчик товаров на странице корзины
-        String cartCounter = driver.findElement(By.cssSelector("#basket-default-prod-count2")).getText().trim();
-        System.out.println("Счетчик товаров в корзине = " + cartCounter);
-    }
-
-    public static void checkCartPrice () {
-
-        // Находим и выводим общую стоимость товаров в корзине со скидкой
-        String price = driver.findElement(By.cssSelector("#basket-default-sumprice-discount")).getText().trim();
-        System.out.println("Цена = " + price);
     }
 }
---
+++ src/main/java/ru/merion/aqa/lesson7/page/CartPage.java
@@ -0,0 +1,31 @@
+package ru.merion.aqa.lesson7.page;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class CartPage {
+    private final WebDriver driver;
+    private static By cartIconLocator = (By.cssSelector(".j-cart-count"));
+
+    public CartPage(WebDriver driver) {
+        this.driver = driver;
+    }
+    public void open() {
+
+        // Кликаем по иконке корзины для перехода на страницу корзины
+        driver.findElement(cartIconLocator).click();
+    }
+    public void checkCartCounter () {
+
+        // Находим и выводим счётчик товаров на странице корзины
+        String cartCounter = driver.findElement(By.cssSelector("#basket-default-prod-count2")).getText().trim();
+        System.out.println("Счетчик товаров в корзине = " + cartCounter);
+    }
+
+    public void checkCartPrice () {
+
+        // Находим и выводим общую стоимость товаров в корзине со скидкой
+        String price = driver.findElement(By.cssSelector("#basket-default-sumprice-discount")).getText().trim();
+        System.out.println("Цена = " + price);
+    }
+}
