5c938abb402ce9df5a273d8fca70815f4b2240aa
Andrey_O <a_reshkin@inbox.ru>
2025-09-30 23:58:55 +0300

    Update Project: - [Lesson 7 - Page Object]

--- src/main/java/ru/merion/aqa/lesson7/LabirintScenario.java
+++ src/main/java/ru/merion/aqa/lesson7/LabirintScenario.java
@@ -15,17 +15,27 @@ public class LabirintScenario {
         // Создаём экземпляр Chrome драйвера через фабрику
         WebDriver driver = WebDriverFactory.create("chrome");
 
+        // Создаём объект главной страницы и передаём в него драйвер
         MainPage mainPage = new MainPage(driver);
+        // Открываем главную страницу сайта Лабиринт
         mainPage.open();
+        // Выполняем поиск по ключевому слову "Java"
         mainPage.searchFor("Java");
 
+        // Создаём объект страницы результатов поиска
         ResultPage resultPage = new ResultPage(driver);
+        // Добавляем все доступные товары из результатов поиска в корзину
         resultPage.addAllItemsToCart();
+        // Проверяем счётчик товаров на иконке корзины
         resultPage.checkIconText();
 
+        // Создаём объект страницы корзины
         CartPage cartPage = new CartPage(driver);
+        // Открываем страницу корзины (кликаем по иконке корзины)
         cartPage.open();
+        // Проверяем и выводим количество товаров в корзине
         cartPage.checkCartCounter();
+        // Проверяем и выводим общую стоимость товаров в корзине
         cartPage.checkCartPrice();
 
         // Закрываем браузер и завершаем сессию WebDriver
