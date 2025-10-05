36276ea5491207ad47ce17de031a5dd27d74c63e
Andrey_O <a_reshkin@inbox.ru>
2025-10-05 22:21:31 +0300

    Update Project: - [Lesson 7 - Page Object]

--- src/main/java/ru/merion/aqa/lesson7/LabirintScenario.java
+++ src/main/java/ru/merion/aqa/lesson7/LabirintScenario.java
@@ -35,7 +35,8 @@ public class LabirintScenario {
                 .checkCartCounter()
                 .checkCartPrice();
 
+        // Закрываем браузер и завершаем сессию WebDriver
+        driver.quit();
 
     }
-
 }
