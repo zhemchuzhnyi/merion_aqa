1380d2819b0697372a04a44a463bdec514f25489
Andrey_O <a_reshkin@inbox.ru>
2025-10-09 22:52:02 +0300

    Update Project: - [Lesson 7 - Page Object]

--- src/main/java/ru/merion/aqa/lesson7/page/HeaderElement.java
+++ src/main/java/ru/merion/aqa/lesson7/page/HeaderElement.java
@@ -6,43 +6,72 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+/**
+ * Класс представляет шапку (header) сайта - верхнюю часть страницы с поиском и корзиной.
+ * Реализует паттерн Page Object для работы с элементами header'а.
+ */
 public class HeaderElement {
+    // Хранит ссылку на WebDriver для взаимодействия с браузером
     private final WebDriver driver;
 
+    // Аннотация @FindBy указывает Selenium, как найти элемент на странице
+    // CSS селектор "#searchform" находит форму поиска по её id
     @FindBy(css = "#searchform")
     private WebElement form;
 
+    // CSS селектор "#search-field" находит поле ввода для поиска по его id
     @FindBy(css = "#search-field")
     private WebElement seachField;
 
+    // CSS селектор ".j-cart-count" находит иконку корзины со счётчиком товаров по классу
     @FindBy(css = ".j-cart-count")
     private WebElement cartIcon;
 
-
+    /**
+     * Конструктор класса - создаёт объект HeaderElement.
+     * @param driver - WebDriver для управления браузером
+     */
     public HeaderElement(WebDriver driver) {
         this.driver = driver;
+        // PageFactory инициализирует все элементы, помеченные @FindBy
+        // ОШИБКА: должно быть initElements(driver, this), а не HeaderElement.class
         PageFactory.initElements(driver, HeaderElement.class);
     }
 
-    public ResultPage searchFor (String term) {
-        // Находим форму поиска на странице
-        // Находим поле ввода поиска и вводим текст (например "Java")
+    /**
+     * Метод выполняет поиск на сайте.
+     * @param term - текст для поиска (например, "Java")
+     * @return объект ResultPage - страницу с результатами поиска
+     */
+    public ResultPage searchFor(String term) {
+        // Вводим текст поиска в поле ввода
         seachField.sendKeys(term);
-        // Отправляем форму поиска
+
+        // Отправляем форму (аналог нажатия Enter или кнопки "Искать")
         form.submit();
+
+        // Создаём и возвращаем объект страницы с результатами
         return new ResultPage(driver);
     }
 
+    /**
+     * Метод получает текст из счётчика корзины.
+     * @return количество товаров в корзине в виде строки (например, "3")
+     */
     public String getIconText() {
-
-        // Получаем и выводим количество товаров из счётчика на иконке корзины
+        // Получаем текст из элемента (число товаров в корзине)
         return cartIcon.getText();
     }
 
+    /**
+     * Метод кликает по иконке корзины для перехода на страницу корзины.
+     * @return объект CartPage - страницу корзины
+     */
     public CartPage clickCartIcon() {
-        // Кликаем по иконке корзины для перехода на страницу корзины
+        // Выполняем клик по иконке корзины
         cartIcon.click();
+
+        // Создаём и возвращаем объект страницы корзины
         return new CartPage(driver);
     }
-
 }
