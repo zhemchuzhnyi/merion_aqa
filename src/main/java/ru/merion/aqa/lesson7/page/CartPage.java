f6c2e0d11d8d51e9d0195fd5b3faafaf736f8763
Andrey_O <a_reshkin@inbox.ru>
2025-10-09 23:29:44 +0300

    Update Project: - [Lesson 7 - Page Object]

--- src/main/java/ru/merion/aqa/lesson7/page/CartPage.java
+++ src/main/java/ru/merion/aqa/lesson7/page/CartPage.java
@@ -2,8 +2,16 @@ package ru.merion.aqa.lesson7.page;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class CartPage extends BasePage{
+
+    @FindBy(css = "#basket-default-sumprice-discount")
+    private WebElement productCounter;
+
+    @FindBy(css = "#basket-default-prod-count2")
+    private WebElement totalPrice;
 
     public CartPage(WebDriver driver) {
         super(driver);
@@ -12,13 +20,13 @@ public class CartPage extends BasePage{
     public String getCartCounter () {
 
         // Находим и выводим счётчик товаров на странице корзины
-        return driver.findElement(By.cssSelector("#basket-default-prod-count2")).getText().trim();
+        return productCounter.getText().trim();
     }
 
     public String getCartPrice () {
 
         // Находим и выводим общую стоимость товаров в корзине со скидкой
-        return driver.findElement(By.cssSelector("#basket-default-sumprice-discount")).getText().trim();
+        return totalPrice.getText().trim();
 
     }
 }
