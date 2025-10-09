ce22f62a1bfdb37da41e9a0c1c89ff3749b29cba
Andrey_O <a_reshkin@inbox.ru>
2025-10-09 22:50:30 +0300

    Update Project: - [Lesson 7 - Page Object]

--- src/main/java/ru/merion/aqa/lesson7/page/HeaderElement.java
+++ src/main/java/ru/merion/aqa/lesson7/page/HeaderElement.java
@@ -3,21 +3,31 @@ package ru.merion.aqa.lesson7.page;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class HeaderElement {
     private final WebDriver driver;
 
-    private static By cartIconLocator = (By.cssSelector(".j-cart-count"));
+    @FindBy(css = "#searchform")
+    private WebElement form;
+
+    @FindBy(css = "#search-field")
+    private WebElement seachField;
+
+    @FindBy(css = ".j-cart-count")
+    private WebElement cartIcon;
+
 
     public HeaderElement(WebDriver driver) {
         this.driver = driver;
+        PageFactory.initElements(driver, HeaderElement.class);
     }
 
     public ResultPage searchFor (String term) {
         // Находим форму поиска на странице
-        WebElement form = driver.findElement(By.cssSelector("#searchform"));
         // Находим поле ввода поиска и вводим текст (например "Java")
-        form.findElement(By.cssSelector("#search-field")).sendKeys(term);
+        seachField.sendKeys(term);
         // Отправляем форму поиска
         form.submit();
         return new ResultPage(driver);
@@ -26,12 +36,12 @@ public class HeaderElement {
     public String getIconText() {
 
         // Получаем и выводим количество товаров из счётчика на иконке корзины
-        return driver.findElement(cartIconLocator).getText();
+        return cartIcon.getText();
     }
 
     public CartPage clickCartIcon() {
         // Кликаем по иконке корзины для перехода на страницу корзины
-        driver.findElement(cartIconLocator).click();
+        cartIcon.click();
         return new CartPage(driver);
     }
 
