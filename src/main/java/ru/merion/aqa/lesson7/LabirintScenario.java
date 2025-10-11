0fead59a2cf78e92c0878a57a01074ffa036a1fa
Andrey_O <a_reshkin@inbox.ru>
2025-10-11 23:35:55 +0300

    Update Project: - [Lesson 7 - Page Object]

--- src/main/java/ru/merion/aqa/lesson7/LabirintScenario.java
+++ src/main/java/ru/merion/aqa/lesson7/LabirintScenario.java
@@ -35,6 +35,13 @@ public class LabirintScenario {
         String price = cartPage.getCartCounter();
         String counter = cartPage.getCartPrice();
 
+        System.out.println("price : " + price);
+        System.out.println("counter: " + counter);
+
+        // Закрываем браузер и завершаем сессию WebDriver
+        driver.quit();
+
+
 
     }
 }
