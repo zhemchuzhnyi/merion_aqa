2ac7caf8152bc7da9388a07c7de36b03c07b81bf
Andrey_O <a_reshkin@inbox.ru>
2025-10-13 22:46:48 +0300

    Update Project: - [practice dz]

--- src/main/java/ru/merion/aqa/practiceDZ3/Task1.java
+++ src/main/java/ru/merion/aqa/practiceDZ3/Task1.java
@@ -1,8 +1,12 @@
 package ru.merion.aqa.practiceDZ3;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Task1 {
 
     public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
 
     }
 }
