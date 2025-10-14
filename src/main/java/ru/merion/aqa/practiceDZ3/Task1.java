619f71161bb95ef3366834ac954174c9b65806e2
Andrey_O <a_reshkin@inbox.ru>
2025-10-14 23:11:02 +0300

    Update Project: - [practice dz]

--- src/main/java/ru/merion/aqa/practiceDZ3/Task1.java
+++ src/main/java/ru/merion/aqa/practiceDZ3/Task1.java
@@ -1,6 +1,8 @@
 package ru.merion.aqa.practiceDZ3;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Task1 {
@@ -8,6 +10,7 @@ public class Task1 {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
         driver.get("http://uitestingplayground.com/textinput");
+        WebElement element = driver.findElement(By.cssSelector());
 
         driver.quit();
 
