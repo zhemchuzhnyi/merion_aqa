e9f6759c9b86951dffa897d4c192404aeeb9e62d
Andrey_O <a_reshkin@inbox.ru>
2025-10-14 23:11:38 +0300

    Update Project: - [practice dz]

--- src/main/java/ru/merion/aqa/practiceDZ3/Task3.java
+++ src/main/java/ru/merion/aqa/practiceDZ3/Task3.java
@@ -1,12 +1,16 @@
 package ru.merion.aqa.practiceDZ3;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Task3 {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
         driver.get("https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html");
+        WebElement element = driver.findElement(By.cssSelector(""));
+
 
 
         driver.quit();
