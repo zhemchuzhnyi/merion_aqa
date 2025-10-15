73e6a7379a25c9d1cd7b434b2f1fdaca409db7f8
Andrey_O <a_reshkin@inbox.ru>
2025-10-15 21:18:38 +0300

    Update Project: - [practice dz]

--- src/main/java/ru/merion/aqa/practiceDZ3/Task3/Task3.java
+++ src/main/java/ru/merion/aqa/practiceDZ3/Task3/Task3.java
@@ -5,6 +5,24 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+/**
+ * Скрипт на калькулятор
+ *
+ * Напишите скрипт. Шаги:
+ *
+ * Открыть страницу https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html
+ * В поле ввода по локатору
+ * #delay
+ *  ввести значение 45
+ * Нажать на кнопки
+ * 7
+ * +(плюс)
+ * 8
+ * =
+ */
+
+// TODO - переделать в PageObject
+
 public class Task3 {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
