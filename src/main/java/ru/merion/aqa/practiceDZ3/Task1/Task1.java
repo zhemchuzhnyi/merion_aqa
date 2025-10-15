ac4320d3bf9e43f24d746d2cde5b9f3396a2f196
Andrey_O <a_reshkin@inbox.ru>
2025-10-15 20:49:10 +0300

    Update Project: - [practice dz]

--- src/main/java/ru/merion/aqa/practiceDZ3/Task1/Task1.java
+++ src/main/java/ru/merion/aqa/practiceDZ3/Task1/Task1.java
@@ -5,10 +5,13 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.time.Duration;
+
 public class Task1 {
 
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(4));
         driver.get("http://uitestingplayground.com/textinput");
         driver.findElement(By.cssSelector(".form-control")).sendKeys("Merion");
         driver.findElement(By.cssSelector(".btn-primary")).click();
