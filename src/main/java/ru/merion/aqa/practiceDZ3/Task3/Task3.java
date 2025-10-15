e13e655819e59aab590b838248386b6ce57c12f3
Andrey_O <a_reshkin@inbox.ru>
2025-10-15 21:29:43 +0300

    Update Project: - [practice dz]

--- src/main/java/ru/merion/aqa/practiceDZ3/Task3/Task3.java
+++ src/main/java/ru/merion/aqa/practiceDZ3/Task3/Task3.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.time.Duration;
+
 /**
  * Скрипт на калькулятор
  *
@@ -26,8 +28,16 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Task3 {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(4));
+
         driver.get("https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html");
-        WebElement element = driver.findElement(By.cssSelector(""));
+        driver.findElement(By.cssSelector("#delay")).clear();
+        driver.findElement(By.cssSelector("#delay")).sendKeys("45");
+
+        driver.findElement(By.xpath("//span[text() = '7']")).click();
+        driver.findElement(By.xpath("//span[text() = '+']")).click();
+        driver.findElement(By.xpath("//span[text() = '8']")).click();
+        driver.findElement(By.xpath("//span[text() = '=']")).click();
 
 
 
