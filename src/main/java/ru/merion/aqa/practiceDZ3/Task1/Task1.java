5fd4c6aa113f28290567e5aa3a5501f67c43e760
Andrey_O <a_reshkin@inbox.ru>
2025-10-15 20:51:19 +0300

    Update Project: - [practice dz]

--- src/main/java/ru/merion/aqa/practiceDZ3/Task1/Task1.java
+++ src/main/java/ru/merion/aqa/practiceDZ3/Task1/Task1.java
@@ -7,6 +7,17 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
 
+/**
+ * Переименовать кнопку
+ *
+ * Напишите скрипт. Шаги:
+ *
+ * Перейти на сайт http://uitestingplayground.com/textinput
+ * Указать в поле ввода текст "Merion"
+ * Нажать на синюю кнопку
+ * Получить текст кнопки и вывести в консоль (Merion)
+ */
+
 public class Task1 {
 
     public static void main(String[] args) {
