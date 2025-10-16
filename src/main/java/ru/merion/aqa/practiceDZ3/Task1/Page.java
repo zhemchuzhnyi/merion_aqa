f27b3a6805135ac6f7764dbe3944074b5433a736
Andrey_O <a_reshkin@inbox.ru>
2025-10-16 23:28:36 +0300

    Update Project: - [practice dz]

--- src/main/java/ru/merion/aqa/practiceDZ3/Task1/Page.java
+++ src/main/java/ru/merion/aqa/practiceDZ3/Task1/Page.java
@@ -1,5 +1,11 @@
 package ru.merion.aqa.practiceDZ3.Task1;
 
+import org.openqa.selenium.WebDriver;
+
 public class Page {
+    public class TextInputPage{
+        private WebDriver driver;
+    }
+
 
 }
