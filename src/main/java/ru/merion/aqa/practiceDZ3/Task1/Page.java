1dc70b03790518b396cbbe6c6ced2ee621e28861
Andrey_O <a_reshkin@inbox.ru>
2025-10-16 23:33:28 +0300

    Update Project: - [practice dz]

--- src/main/java/ru/merion/aqa/practiceDZ3/Task1/Page.java
+++ src/main/java/ru/merion/aqa/practiceDZ3/Task1/Page.java
@@ -2,6 +2,7 @@ package ru.merion.aqa.practiceDZ3.Task1;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class Page {
     public class TextInputPage{
@@ -9,6 +10,14 @@ public class Page {
 
         private By inputField = By.cssSelector(".form-control");
         private By primaryButton = By.cssSelector(".btn-primary");
+
+        public TextInputPage(WebDriver driver) {
+            this.driver = driver;
+        }
+        public WebElement TextInputPage open() {
+            driver.get("http://uitestingplayground.com/textinput");
+            return this;
+        }
 
     }
 }
