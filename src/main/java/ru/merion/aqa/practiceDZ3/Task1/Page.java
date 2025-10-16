d386e660548bcdace18569dec33cef006f2e43ae
Andrey_O <a_reshkin@inbox.ru>
2025-10-16 23:30:37 +0300

    Update Project: - [practice dz]

--- src/main/java/ru/merion/aqa/practiceDZ3/Task1/Page.java
+++ src/main/java/ru/merion/aqa/practiceDZ3/Task1/Page.java
@@ -1,11 +1,14 @@
 package ru.merion.aqa.practiceDZ3.Task1;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class Page {
     public class TextInputPage{
         private WebDriver driver;
+
+        private By inputField = By.ByCssSelector(".form-control");
+        private By primaryButton = By.cssSelector(".btn-primary");
+
     }
-
-
 }
