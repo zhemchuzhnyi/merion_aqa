35947085b5dbe7ed07743b881a0b2f91d21719d4
Andrey_O <a_reshkin@inbox.ru>
2025-10-17 18:43:29 +0300

    Update Project: - [practice dz]

--- src/main/java/ru/merion/aqa/practiceDZ3/Task3/Task3.java
+++ src/main/java/ru/merion/aqa/practiceDZ3/Task3/Task3.java
@@ -1,54 +1,24 @@
 package ru.merion.aqa.practiceDZ3.Task3;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Wait;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.time.Duration;
-
-/**
- * Скрипт на калькулятор
- *
- * Напишите скрипт. Шаги:
- *
- * Открыть страницу https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html
- * В поле ввода по локатору
- * #delay
- *  ввести значение 45
- * Нажать на кнопки
- * 7
- * +(плюс)
- * 8
- * =
- */
+import ru.merion.aqa.WebDriverFactory;
 
 public class Task3 {
-    private static final Logger log = LoggerFactory.getLogger(Task3.class);
 
     public static void main(String[] args) {
-
         int timeout = 45;
-        WebDriver driver = new ChromeDriver();
 
-        MathsPage page = new MathsPage(driver);
+        WebDriver driver = WebDriverFactory.create("chrome");
 
-        page.open()
-                .clearDelay()
-                .setDelay(timeout)
-                .clickNumber7()
-                .clickSign()
-                .clickNumber8()
-                .clickEqual();
+        CalculatorPage calculatorPage = new CalculatorPage(driver);
 
-        String result = page.getResult(timeout);
-        System.out.println("Result: " + result);
+        calculatorPage.open();
+        calculatorPage.setDelay(timeout);
+        calculatorPage.performCalculation("7", "+", "8");
+        calculatorPage.waitForCalculationComplete(timeout);
 
+        String result = calculatorPage.getResult();
+        System.out.println("Результат сложения: " + result);
 
         driver.quit();
     }
