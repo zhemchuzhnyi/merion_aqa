1e25e9d40d2bae98e79dc4d686a73e15bdac5bf1
Andrey_O <a_reshkin@inbox.ru>
2025-10-17 16:15:07 +0300

    Update Project: - [practice dz]

--- src/main/java/ru/merion/aqa/practiceDZ3/Task2/Task2.java
+++ src/main/java/ru/merion/aqa/practiceDZ3/Task2/Task2.java
@@ -1,64 +1,37 @@
 package ru.merion.aqa.practiceDZ3.Task2;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
 
 /**
- * Скрипт заполнения формы
- *
- * Напишите скрипт. Шаги:
- *
- * Открыть страницу https://bonigarcia.dev/selenium-webdriver-java/data-types.html
- *
- * Заполнить форму значениями
- *
- * Поле	Значение
- * First name	Иван
- * Last name	Петров
- * Address	Ленина, 55-3
- * Zip code	*оставить пустым
- * City	Москва
- * Country	Россия
- * E-mail	*оставить пустым
- * Phone number	*оставить пустым
- * Job position	QA
- * Company	Merion
- * Нажать кнопку Submit
- *
- * Вывести в консоль цвет полей
- * Zip code
- * ,
- * E-mail
- *  и
- * Phone
- *  (background-color)
+ * Тест заполнения формы с использованием Page Object паттерна
  */
-
 public class Task2 {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(4));
 
+        // Создаем Page Object
         DataTypesPage page = new DataTypesPage(driver);
 
+        // Выполняем тест
         page.open()
                 .fillFirstName("Иван")
-                        .fillLastName("Петров")
-                                .fillAddress("Ленина, 55-3")
-                                        .fillCity("Москва")
-                                                .fillCountry("Россия")
-                                                        .fillJobPosition("QA")
-                                                                .fillCompany("Merion");
+                .fillLastName("Петров")
+                .fillAddress("Ленина, 55-3")
+                .fillCity("Москва")
+                .fillCountry("Россия")
+                .fillJobPosition("QA")
+                .fillCompany("Merion")
+                .submit();
 
-        System.out.println("Цвет Zip Code: " + page.getZipCodeResult());
-        System.out.println("Цвет Email: " + page.getEmailResult());
-        System.out.println("Цвет Phone: " + page.getPhoneResult());
+        // Проверяем цвета полей
+        System.out.println("Цвет Zip Code: " + page.getZipCodeColor());
+        System.out.println("Цвет E-mail: " + page.getEmailColor());
+        System.out.println("Цвет Phone: " + page.getPhoneColor());
 
         driver.quit();
-
     }
 }
