2d3b166b7000d53bba9e139e7d9ad362bd9a9f46
Andrey_O <a_reshkin@inbox.ru>
2025-10-17 17:40:37 +0300

    Update Project: - [practice dz]

---
+++ src/main/java/ru/merion/aqa/practiceDZ3/Task3/MathsPage.java
@@ -0,0 +1,18 @@
+package ru.merion.aqa.practiceDZ3.Task3;
+
+import org.openqa.selenium.By;
+
+import java.sql.Driver;
+
+public class MathsPage {
+    private Driver driver;
+
+    // Локаторы
+    private By del = By.cssSelector("#delay");
+
+    private By num7 = By.xpath("//span[text() = '7']");
+    private By sign = By.xpath("//span[text() = '+']");
+    private By num8 = By.xpath("//span[text() = '8']");
+    private By equal = By.xpath("//span[text() = '=']");
+
+}
