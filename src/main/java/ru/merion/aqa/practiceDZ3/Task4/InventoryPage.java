6be05799bb2d3c43952c565cd35a4a9c487aa0af
Andrey_O <a_reshkin@inbox.ru>
2025-10-18 14:28:38 +0300

    Update Project: - [practice dz]

--- src/main/java/ru/merion/aqa/practiceDZ3/Task4/InventoryPage.java
+++ src/main/java/ru/merion/aqa/practiceDZ3/Task4/InventoryPage.java
@@ -3,39 +3,30 @@ package ru.merion.aqa.practiceDZ3.Task4;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.time.Duration;
 import java.util.List;
 
-public class InventoryPage {
+class InventoryPage {
     private WebDriver driver;
-    private WebElement wait;
-
-    @FindBy(css = ".inventory_item")
-    private List<WebElement> inventoryItems;
-
-    @FindBy(css = ".inventory_item")
-    private WebElement itemName;
+    private By inventoryItems = By.cssSelector(".inventory_item");
+    private By itemName = By.cssSelector(".inventory_item_name");
+    private By addButton = By.cssSelector("button");
 
     public InventoryPage(WebDriver driver) {
         this.driver = driver;
-        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        PageFactory.initElements(driver, this);
     }
 
-    public void addItemToCart (Set<String> itemNames) {
-        List<WebElement> items = driver.findElements(By.cssSelector(".inventory_item"));
+    public void addItemsToCart() {
+        List<WebElement> items = driver.findElements(inventoryItems);
         for (WebElement item : items) {
-            String productName = item.findElement(By.cssSelector(".inventory_item_name")).getText();
+            String productName = item.findElement(itemName).getText();
             if (itemNames.contains(productName)) {
-                item.findElement(By.cssSelector("button")).click();
+                item.findElement(addButton).click();
             }
         }
     }
-    public void goToCArt() {
+
+    public void goToCart() {
         driver.get("https://www.saucedemo.com/cart.html");
     }
 }
--- src/main/java/ru/merion/aqa/practiceDZ3/Task4/Task4.java
+++ src/main/java/ru/merion/aqa/practiceDZ3/Task4/Task4.java
@@ -1,88 +1,43 @@
-package ru.merion.aqa.practiceDZ3.Task4;
+package ru.merion.aqa.practiceDZ2;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+import ru.merion.aqa.WebDriverFactory;
+import ru.merion.aqa.practiceDZ2.pages.*;
 
 import java.time.Duration;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
-/**
- * Напишите скрипт для работы с интернет-магазином. Шаги
- *
- * Открыть сайт магазина https://www.saucedemo.com/
- * Авторизоваться под пользователем
- * standard_user
- * Добавить в корзину товары:
- * Sauce Labs Backpack
- * Sauce Labs Bolt T-Shirt
- * Sauce Labs Onesie
- * Перейти в корзину
- * Нажать Checkout
- * Заполнить форму данными:
- * Имя
- * Фамиля
- * Почтовый индекс
- * Нажать continue
- * Прочитать со стрницы итоговую стоимость (
- * Total
- *  )
- * Закрыть браузер
- * Вывести в консоль итоговую стоимость
- */
+public class Task6WithPageObject {
 
-// TODO - переделать в PageObject
-
-public class Task4 {
     public static void main(String[] args) {
-
         Set<String> itemNames = new HashSet<>();
         itemNames.add("Sauce Labs Backpack");
         itemNames.add("Sauce Labs Bolt T-Shirt");
         itemNames.add("Sauce Labs Onesie");
 
-        WebDriver driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-/**
-        driver.get("https://www.saucedemo.com/");
-        driver.findElement(By.cssSelector("#user-name")).sendKeys("standard_user");
-        driver.findElement(By.cssSelector("#password")).sendKeys("secret_sauce");
-        driver.findElement(By.cssSelector("#login-button")).click();
-
-        List<WebElement> items = driver.findElements(By.cssSelector(".inventory_item"));
-        for (WebElement item : items) {
-            String productName = item.findElement(By.cssSelector(".inventory_item_name")).getText();
-            if (itemNames.contains(productName)) {
-                item.findElement(By.cssSelector("button")).click();
-            }
-        }
-
-        driver.get("https://www.saucedemo.com/cart.html");
-        driver.findElement(By.cssSelector("#checkout")).click();
-
-        driver.findElement(By.cssSelector("#first-name")).sendKeys("Alex");
-        driver.findElement(By.cssSelector("#last-name")).sendKeys("Smith");
-        driver.findElement(By.cssSelector("#postal-code")).sendKeys("404546");
-
-        driver.findElement(By.cssSelector("#continue")).click();
-        String total = driver.findElement(By.cssSelector(".summary_total_label")).getText();
-**/
-        driver.get("https://www.saucelabs.com/");
+        WebDriver driver = WebDriverFactory.create("chrome");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(4));
 
         LoginPage loginPage = new LoginPage(driver);
+        InventoryPage inventoryPage = new InventoryPage(driver);
+        CartPage cartPage = new CartPage(driver);
+        CheckoutPage checkoutPage = new CheckoutPage(driver);
+
+        loginPage.open();
         loginPage.login("standard_user", "secret_sauce");
 
-        InventoryPage inventoryPage = new InventoryPage(driver);
-        inventoryPage.addItemsToCart(itemsToAdd);
-        System.out.println("✓ Товары добавлены в корзину");
+        inventoryPage.addItemsToCart(itemNames);
 
+        cartPage.open();
+        cartPage.clickCheckout();
 
-        driver.close();
+        checkoutPage.fillPersonalInfo("Alex", "Smith", "12345");
+        checkoutPage.clickContinue();
 
-        System.out.println("Итоговая стоимость: " + total);
+        String total = checkoutPage.getTotalAmount();
+        driver.quit();
 
+        System.out.println("Результат: " + total);
     }
 }
