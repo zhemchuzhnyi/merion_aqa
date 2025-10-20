ae180556a4810a1cf0018e9105f26ccc1bebee09
Andrey_O <a_reshkin@inbox.ru>
2025-10-20 23:28:29 +0300

    Update Project: - [practice dz]

--- src/main/java/ru/merion/aqa/practiceDZ3_1/Task2/Task2.java
+++ src/main/java/ru/merion/aqa/practiceDZ3_1/Task2/Task2.java
@@ -1,11 +1,11 @@
-package ru.merion.aqa.practiceDZ3_1.Task2_1;
+package ru.merion.aqa.practiceDZ3_1.Task2;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
 
-public class Task2_1 {
+public class Task2 {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(4));
