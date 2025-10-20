d294faff2c8a2342b506ae7083aa7141ea0a80eb
Andrey_O <a_reshkin@inbox.ru>
2025-10-20 23:53:53 +0300

    Update Project: - [practice dz]

---
+++ src/main/java/ru/merion/aqa/practiceDZ3_1/Task2/TextInputPage.java
@@ -0,0 +1,24 @@
+package ru.merion.aqa.practiceDZ3_1.Task2;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class TextInputPage {
+    private final WebDriver driver;
+
+    public TextInputPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public TextInputPage open() {
+        driver.get("http://uitestingplayground.com/textinput");
+        return this;
+    }
+    public TextInputPage setButtonName(String name) {
+        driver.findElement(By.cssSelector("#newButtonName")).sendKeys("newName");
+        driver.findElement(By.cssSelector("#updatingButton")).click();
+        return this;
+    }
+    public String getButtonText() {return driver.findElement(By.cssSelector("#updatingButton")).getText();}
+
+}
