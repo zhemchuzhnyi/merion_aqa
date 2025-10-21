1fc76d2626a9e616ba069a42ac5fc1560b1a2331
Andrey_O <a_reshkin@inbox.ru>
2025-10-21 23:00:50 +0300

    Update Project: - [practice dz]

---
+++ src/main/java/ru/merion/aqa/practiceDZ3_1/Task4/RegisterPage.java
@@ -0,0 +1,23 @@
+package ru.merion.aqa.practiceDZ3_1.Task4;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class RegisterPage {
+    private final WebDriver driver;
+
+    public RegisterPage(WebDriver driver) {this.driver = driver;}
+
+    public RegisterPage open() {
+        driver.get("https://bonigarcia.dev/selenium-webdriver-java/data-types.html");
+        return this;
+    }
+
+    public void set(String field, String value) {
+        driver.findElement(By.cssSelector("[name=" + field + "]")).sendKeys(value);
+    }
+
+    public void submitForm() {
+        driver.findElement(By.cssSelector("[type=submit]")).click();
+    }
+}
---
+++ src/main/java/ru/merion/aqa/practiceDZ3_1/Task4/Task4.java
@@ -0,0 +1,26 @@
+package ru.merion.aqa.practiceDZ3_1.Task4;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Task4 {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+
+        RegisterPage page = new RegisterPage(driver).open();
+
+        page.set("first-name", "Иван");
+        page.set(";ast-name", "Иванов");
+        page.set("job-position", "QA");
+        page.set("address", "Ленина 55-3");
+        page.set("city", "Moscow");
+        page.set("country", "Russia");
+        page.set("company", "Merion");
+
+        page.submitForm();
+
+        String phone_bg = page.getCssProperty("#phone", "background-color");
+        String email_bg = page.getCssProperty("#e-mail", "background-color");
+        String zip_code_bg = page.getCssProperty("#zip-code", "background-color");
+    }
+}
