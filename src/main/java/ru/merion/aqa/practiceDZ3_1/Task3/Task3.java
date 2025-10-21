2d35cda823ae313053ffb9380e1be7494440b5b0
Andrey_O <a_reshkin@inbox.ru>
2025-10-21 22:40:36 +0300

    Update Project: - [practice dz]

--- src/main/java/ru/merion/aqa/practiceDZ3_1/Task3/Task3.java
+++ src/main/java/ru/merion/aqa/practiceDZ3_1/Task3/Task3.java
@@ -2,6 +2,7 @@ package ru.merion.aqa.practiceDZ3_1.Task3;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import ru.merion.aqa.WebDriverFactory;
@@ -11,12 +12,11 @@ import java.time.Duration;
 public class Task3 {
 
     public static void main(String[] args) {
-        WebDriver driver = WebDriverFactory.create("chrome");
-        driver.get("https://bonigarcia.dev/selenium-webdriver-java/loading-images.html");
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(16));
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#award")));
+        WebDriver driver = new ChromeDriver();
 
-        String src = driver.findElement(By.cssSelector("#award")).getAttribute("src");
+        ImageGalleryPage gallery = new ImageGalleryPage(driver).open();
+        String src = gallery.getImageProperty(2,"src");
+
         System.out.println("Атрибут src 3й картинки: " + src);
 
         driver.quit();
