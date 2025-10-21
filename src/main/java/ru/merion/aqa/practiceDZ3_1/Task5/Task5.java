888b93edabee1a3220db468bcc88add4cd9cab4d
Andrey_O <a_reshkin@inbox.ru>
2025-10-21 23:14:51 +0300

    Update Project: - [practice dz]

---
+++ src/main/java/ru/merion/aqa/practiceDZ3_1/Task5/Task5.java
@@ -0,0 +1,24 @@
+package ru.merion.aqa.practiceDZ3_1.Task5;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import ru.merion.aqa.practiceDZ3.Task3.CalculatorPage;
+
+public class Task5 {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        CalculatorPage calculator = new CalculatorPage(driver).open();
+
+        calculator.setDelay(10);
+        calculator.press_7();
+        calculator.press_plus();
+        calculator.press_8();
+        calculator.press_eq();
+
+        String result = calculator.getResult();
+        System.out.println(result);
+
+        driver.quit();
+
+    }
+}
