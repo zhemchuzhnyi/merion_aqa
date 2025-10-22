eb849f58ef8231ca375d67df407a74e43c9c6688
Andrey_O <a_reshkin@inbox.ru>
2025-10-22 21:00:15 +0300

    Update Project: - [practice dz]

---
+++ src/main/java/ru/merion/aqa/practiceDZ3_1/Task6/CatalogPage.java
@@ -0,0 +1,11 @@
+package ru.merion.aqa.practiceDZ3_1.Task6;
+
+import org.openqa.selenium.WebDriver;
+
+public class CatalogPage {
+    private final WebDriver driver;
+
+    public CatalogPage(WebDriver driver) {
+        this.driver = driver;
+    }
+}
