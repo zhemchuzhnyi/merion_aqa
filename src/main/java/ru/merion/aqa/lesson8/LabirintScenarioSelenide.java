011f4fecff721a7b74a3a77520429360bc08670b
Andrey_O <a_reshkin@inbox.ru>
2025-10-23 22:24:48 +0300

    Update Project: - [Selenide]

--- src/main/java/ru/merion/aqa/lesson8/LabirintScenarioSelenide.java
+++ src/main/java/ru/merion/aqa/lesson8/LabirintScenarioSelenide.java
@@ -1,7 +1,21 @@
 package ru.merion.aqa.lesson8;
+import com.codeborne.selenide.WebDriverRunner;
+import org.openqa.selenium.Cookie;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 public class LabirintScenarioSelenide {
     public static void main(String[] args) {
+        open("https://www.labirint.ru/");
+        Cookie cookie = new Cookie("cookie_policy", "1");
+        WebDriverRunner.getWebDriver().manage().addCookie(cookie);
+
+        open("https://www.labirint.ru/");
+        $("#search-field").val("Java");
+
+
+
 
     }
 }
