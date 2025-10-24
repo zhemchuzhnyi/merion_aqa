e83e110533c2e2ee36a160ceba759e218bc643a5
Andrey_O <a_reshkin@inbox.ru>
2025-10-24 14:23:03 +0300

    Update Project: - [Selenide - работа с коллекциями]

---
+++ src/main/java/ru/merion/aqa/lesson9/ConfigDemo.java
@@ -0,0 +1,10 @@
+package ru.merion.aqa.lesson9;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class ConfigDemo {
+
+    public static void main(String[] args) {
+        open("https://habr.com/ru");
+    }
+}
