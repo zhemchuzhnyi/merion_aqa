bf7aa8cdee1521329f221d959b9346d3b0e08574
Andrey_O <a_reshkin@inbox.ru>
2025-10-24 14:46:11 +0300

    Update Project: - [Selenide - работа с коллекциями]

--- src/main/java/ru/merion/aqa/lesson9/ElementsCollectionDemo.java
+++ src/main/java/ru/merion/aqa/lesson9/ElementsCollectionDemo.java
@@ -5,12 +5,13 @@ import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.WebDriverRunner;
 import org.openqa.selenium.Cookie;
 
-import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.CollectionCondition.size;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Selenide.$;
 
 public class ElementsCollectionDemo {
     public static void main(String[] args) {
+
         open("https://www.labirint.ru/");
         Cookie cookie = new Cookie("cookie_policy", "1");
         WebDriverRunner.getWebDriver().manage().addCookie(cookie);
@@ -18,11 +19,9 @@ public class ElementsCollectionDemo {
         open("https://www.labirint.ru/");
         $("#search-field").val("Java").pressEnter();
 
-        ElementsCollection cardsWithBuyButton = $$(".product-card:has(.buy-link)");
-        cardsWithBuyButton.forEach(card -> card.find(".buy-link").click());
+        ElementsCollection cards = $$(".product-card")
+                .shouldHave(size(38));
 
-        $(".j-cart-count").shouldHave(text(String.valueOf(cardsWithBuyButton.size())))
-                .shouldHave(text(String.valueOf(cardsWithBuyButton.size())));
 
 
         Selenide.closeWebDriver();
