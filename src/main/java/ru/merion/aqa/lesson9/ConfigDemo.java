b3e4274f8ab89725e04d45d7e1740f4ea9ee6ccf
Andrey_O <a_reshkin@inbox.ru>
2025-10-24 14:33:27 +0300

    Update Project: - [Selenide - работа с коллекциями]

--- src/main/java/ru/merion/aqa/lesson9/ConfigDemo.java
+++ src/main/java/ru/merion/aqa/lesson9/ConfigDemo.java
@@ -1,10 +1,20 @@
 package ru.merion.aqa.lesson9;
 
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+
 import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.title;
 
 public class ConfigDemo {
 
     public static void main(String[] args) {
+        Configuration.timeout = 1000*8;
+        //Configuration.headless = true; // - не показывает интерфейс
+
         open("https://habr.com/ru");
+        System.out.println(title());
+
+        Selenide.closeWebDriver();
     }
 }
