63d0729564b7817fcc9e0f2fd8eef8fef05b1fa2
Andrey_O <a_reshkin@inbox.ru>
2025-10-27 22:18:53 +0300

    Update Project: - [Selenide - DZ]

---
+++ src/main/java/ru/merion/aqa/selenideDZ_1/Task4.java
@@ -0,0 +1,36 @@
+package ru.merion.aqa.selenideDZ_1;
+
+/*
+Скрипт заполнения формы
+
+Напишите скрипт. Шаги:
+
+Открыть страницу https://bonigarcia.dev/selenium-webdriver-java/data-types.html
+
+Заполнить форму значениями
+
+Поле	Значение
+First name	Иван
+Last name	Петров
+Address	Ленина, 55-3
+Zip code	*оставить пустым
+City	Москва
+Country	Россия
+E-mail	*оставить пустым
+Phone number	*оставить пустым
+Job position	QA
+Company	Merion
+Нажать кнопку Submit
+
+Вывести в консоль цвет полей
+Zip code
+,
+E-mail
+ и
+Phone
+ (background-color)
+ */
+
+public class Task4 {
+    public static void main(String[] args) {}
+}
