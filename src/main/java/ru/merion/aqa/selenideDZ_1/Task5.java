debe4f3dcda621c812f6ede0393d3a232962bcd7
Andrey_O <a_reshkin@inbox.ru>
2025-10-27 23:12:17 +0300

    Update Project: - [Selenide - DZ]

---
+++ src/main/java/ru/merion/aqa/selenideDZ_1/Task5.java
@@ -0,0 +1,24 @@
+package ru.merion.aqa.selenideDZ_1;
+
+/*
+Скрипт на калькулятор
+
+Напишите скрипт. Шаги:
+
+Открыть страницу https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html
+В поле ввода по локатору
+#delay
+ ввести значение 45
+Нажать на кнопки
+7
++(плюс)
+8
+=
+Дождаться результата. Вывести его в консоль.
+ */
+
+public class Task5 {
+    public static void main(String[] args) {
+
+    }
+}
