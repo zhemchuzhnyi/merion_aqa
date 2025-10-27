cb4746623be91d4e55f96f29d5718bae99ac97e3
Andrey_O <a_reshkin@inbox.ru>
2025-10-27 23:15:25 +0300

    Update Project: - [Selenide - DZ]

--- src/main/java/ru/merion/aqa/selenideDZ_1/Task5.java
+++ src/main/java/ru/merion/aqa/selenideDZ_1/Task5.java
@@ -17,8 +17,21 @@ package ru.merion.aqa.selenideDZ_1;
 Дождаться результата. Вывести его в консоль.
  */
 
+import com.codeborne.selenide.Selenide;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
 public class Task5 {
     public static void main(String[] args) {
+        open("https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html");
+        $(By.cssSelector("#delay")).clear();
+        $(By.cssSelector("#delay")).val("10");
+
+        Selenide.closeWebDriver();
+
+
 
     }
 }
