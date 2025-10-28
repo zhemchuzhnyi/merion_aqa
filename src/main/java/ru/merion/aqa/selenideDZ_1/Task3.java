b39963e6f21118f1c4df4ee5c38548258a2479b2
Andrey_O <a_reshkin@inbox.ru>
2025-10-28 22:39:55 +0300

    Update Project: - [Selenide - DZ]

--- src/main/java/ru/merion/aqa/selenideDZ_1/Task3.java
+++ src/main/java/ru/merion/aqa/selenideDZ_1/Task3.java
@@ -12,20 +12,29 @@ src - у 3й картинки
 Вывести значение в консоль
  */
 
+import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.By;
 
 import java.time.Duration;
 
 import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class Task3 {
     public static void main(String[] args) {
+
         open("https://bonigarcia.dev/selenium-webdriver-java/loading-images.html");
-        String picture = $(By.cssSelector("#award")).shouldBe(visible, Duration.ofSeconds(15)).getAttribute("src");
-        System.out.println("Атрибут картинки: " + picture);
+
+        SelenideElement spinner = $("#spinner");
+        spinner.shouldNotBe(visible, Duration.ofSeconds(10));
+
+        ElementsCollection images = $$("#image-container img");
+        String src = images.get(2).attr("src");
+
+        System.out.println("Атрибут картинки: " + src);
+
         Selenide.closeWebDriver();
 
 
