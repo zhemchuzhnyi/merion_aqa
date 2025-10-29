7da7a34fb06819d702c15f7e99126eecc57494c8
Andrey_O <a_reshkin@inbox.ru>
2025-10-29 22:25:45 +0300

    Update Project: - [Selenide - PageObject]

--- src/main/java/ru/merion/aqa/lesson10/midlevel_page/ResultPage.java
+++ src/main/java/ru/merion/aqa/lesson10/midlevel_page/ResultPage.java
@@ -1,11 +1,4 @@
 package ru.merion.aqa.lesson10.midlevel_page;
-
-import com.codeborne.selenide.ElementsCollection;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 
 public class ResultPage {
