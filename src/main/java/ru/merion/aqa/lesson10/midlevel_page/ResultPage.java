ec60ef4e7f00fe0466d8caecc8e9a8f2ab6921ab
Andrey_O <a_reshkin@inbox.ru>
2025-10-29 22:25:27 +0300

    Update Project: - [Selenide - PageObject]

--- src/main/java/ru/merion/aqa/lesson10/midlevel_page/ResultPage.java
+++ src/main/java/ru/merion/aqa/lesson10/midlevel_page/ResultPage.java
@@ -11,19 +11,7 @@ import static com.codeborne.selenide.Selenide.$$;
 public class ResultPage {
 
     public void addAllItemsToCart() {
-        ElementsCollection cardsWithBuyButton = $$(".product-card:has(.buy-link)");
-        cardsWithBuyButton.forEach(card -> card.find(".buy-link").click());
-
-        $(".j-cart-count").shouldHave(text(String.valueOf(cardsWithBuyButton.size())))
-                .shouldHave(text(String.valueOf(cardsWithBuyButton.size())))
-                .click();
-
-
-        int counter = 0;
-        for (WebElement card : cards) {
-            card.findElement(By.cssSelector(".buy-link")).click();
-            counter++;
-        }
+        $$(".product-card:has(.buy-link)").forEach(card -> card.find(".buy-link").click());
 
     }
 
