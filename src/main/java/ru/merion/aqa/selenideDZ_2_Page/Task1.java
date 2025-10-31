bc3d2d09a757b55c05e6a98584582780b4fb800b
Andrey_O <a_reshkin@inbox.ru>
2025-10-31 22:51:04 +0300

    Update Project: - [Selenide DZ_2 - PageObject]

---
+++ src/main/java/ru/merion/aqa/selenideDZ_2_Page/Task1.java
@@ -0,0 +1,20 @@
+package ru.merion.aqa.selenideDZ_2_Page;
+
+/*
+Нажатие на кнопку
+
+Напишите скрипт. Шаги:
+
+Перейти на страницу http://uitestingplayground.com/ajax
+Нажать на синюю кнопку
+Получить текст из зеленой плашки
+Вывести его в консоль (”Data loaded with AJAX get request.”)
+ */
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class Task1 {
+    public static void main(String[] args) {
+        open("http://uitestingplayground.com/ajax");
+    }
+}
