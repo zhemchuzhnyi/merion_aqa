b9b8a5442053be5b0c8b86eb766405d0283b0fff
Andrey_O <a_reshkin@inbox.ru>
2025-10-31 23:07:10 +0300

    Update Project: - [WebDriver DZ_1 - PageObject]

--- src/main/java/ru/merion/aqa/selenideDZ_2_Page/Task1/Task1.java
+++ src/main/java/ru/merion/aqa/selenideDZ_2_Page/Task1/Task1.java
@@ -11,11 +11,22 @@ package ru.merion.aqa.selenideDZ_2_Page.Task1;
 Вывести его в консоль (”Data loaded with AJAX get request.”)
  */
 
+import com.codeborne.selenide.Selenide;
+import org.openqa.selenium.By;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class Task1 {
     public static void main(String[] args) {
-        open("http://uitestingplayground.com/ajax");
 
+        open("http://uitestingplayground.com/ajax");
+        $(By.cssSelector("#ajaxButton")).click();
+        String content = $(By.cssSelector("#content p")).shouldBe(visible, Duration.ofSeconds(16)).getText();
+        System.out.println(content);
+        Selenide.closeWebDriver();
     }
 }
