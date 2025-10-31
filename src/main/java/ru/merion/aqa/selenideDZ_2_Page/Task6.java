4d181a41c9f84b8ae6b57f7cc32941a5cb1597f6
Andrey_O <a_reshkin@inbox.ru>
2025-10-31 22:56:51 +0300

    Update Project: - [Selenide DZ_2 - PageObject]

---
+++ src/main/java/ru/merion/aqa/selenideDZ_2_Page/Task6.java
@@ -0,0 +1,33 @@
+package ru.merion.aqa.selenideDZ_2_Page;
+
+/*
+Напишите скрипт для работы с интернет-магазином. Шаги
+
+Открыть сайт магазина https://www.saucedemo.com/
+Авторизоваться под пользователем
+standard_user
+Добавить в корзину товары:
+Sauce Labs Backpack
+Sauce Labs Bolt T-Shirt
+Sauce Labs Onesie
+Перейти в корзину
+Нажать Checkout
+Заполнить форму данными:
+Имя
+Фамиля
+Почтовый индекс
+Нажать continue
+Прочитать со стрницы итоговую стоимость (
+Total
+ )
+Закрыть браузер
+Вывести в консоль итоговую стоимость `
+ */
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class Task6 {
+    public static void main(String[] args) {
+        open("https://www.saucedemo.com/");
+    }
+}
