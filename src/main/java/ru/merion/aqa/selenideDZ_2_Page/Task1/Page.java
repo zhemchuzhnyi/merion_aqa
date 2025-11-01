440f70f83fd773fa80f4e80ac15637355704c84b
Andrey_O <a_reshkin@inbox.ru>
2025-11-01 23:56:39 +0300

    Update Project: - [WebDriver DZ_1 - PageObject]

--- src/main/java/ru/merion/aqa/selenideDZ_2_Page/Task1/Page.java
+++ src/main/java/ru/merion/aqa/selenideDZ_2_Page/Task1/Page.java
@@ -1,6 +1,22 @@
 package ru.merion.aqa.selenideDZ_2_Page.Task1;
 
-public class Page {
-    open”Data loaded with AJAX get request.”);
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.$;
+import com.codeborne.selenide.SelenideElement;
 
+public class Page {
+
+    private static final String PAGE_URL = "https://example.com";
+
+    // Локатор для контента, загруженного через AJAX
+    private SelenideElement ajaxContent = $("#ajax-content");
+
+    public Page openPage() {
+        open(PAGE_URL);
+        return this;
+    }
+
+    public String getAjaxLoadedText() {
+        return ajaxContent.getText();
+    }
 }
