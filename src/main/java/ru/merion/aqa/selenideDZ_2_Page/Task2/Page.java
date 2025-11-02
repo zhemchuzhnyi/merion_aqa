cf8101544db6d11418930f57e8ec67808f4c96c5
Andrey_O <a_reshkin@inbox.ru>
2025-11-02 21:58:25 +0300

    Update Project: - [WebDriver DZ_1 - PageObject]

---
+++ src/main/java/ru/merion/aqa/selenideDZ_2_Page/Task2/Page.java
@@ -0,0 +1,4 @@
+package ru.merion.aqa.selenideDZ_2_Page.Task2;
+
+public class Page {
+}
--- src/main/java/ru/merion/aqa/selenideDZ_2_Page/Task2/Task2.java
+++ src/main/java/ru/merion/aqa/selenideDZ_2_Page/Task2/Task2.java
@@ -11,10 +11,12 @@ package ru.merion.aqa.selenideDZ_2_Page.Task2;
 Получить текст кнопки и вывести в консоль (Merion)
  */
 
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class Task2 {
     public static void main(String[] args) {
         open("http://uitestingplayground.com/textinput");
+        $(".form-control").val("Merion");
     }
 }
