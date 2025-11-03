0c0f85d49bdbc6c59c41e1186d5b81068b50845c
Andrey_O <a_reshkin@inbox.ru>
2025-11-03 22:47:04 +0300

    Update Project: - [WebDriver DZ_1 - PageObject]

--- src/main/java/ru/merion/aqa/selenideDZ_2_Page/Task4/Task4.java
+++ src/main/java/ru/merion/aqa/selenideDZ_2_Page/Task4/Task4.java
@@ -31,10 +31,12 @@ Phone
  (background-color)
  */
 
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class Task4 {
     public static void main(String[] args) {
         open("https://bonigarcia.dev/selenium-webdriver-java/data-types.html");
+        $("");
     }
 }
