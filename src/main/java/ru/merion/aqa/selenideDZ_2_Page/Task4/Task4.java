f98027df57c60339974e9a97e4e9611dcbdb8193
Andrey_O <a_reshkin@inbox.ru>
2025-11-03 22:48:02 +0300

    Update Project: - [WebDriver DZ_1 - PageObject]

--- src/main/java/ru/merion/aqa/selenideDZ_2_Page/Task4/Task4.java
+++ src/main/java/ru/merion/aqa/selenideDZ_2_Page/Task4/Task4.java
@@ -31,12 +31,32 @@ Phone
  (background-color)
  */
 
+import com.codeborne.selenide.Selenide;
+
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class Task4 {
     public static void main(String[] args) {
         open("https://bonigarcia.dev/selenium-webdriver-java/data-types.html");
-        $("");
+        $(("[name = 'first-name']")).val("Иван");
+        $(("[name = 'last-name']")).val("Петров");
+        $(("[name = 'address']")).val("Ленина, 55-3");
+        $(("[name = 'city']")).val("Москва");
+        $(("[name = 'country']")).val("Россия");
+        $(("[name = 'job-position']")).val("QA");
+        $(("[name = 'company']")).val("Merion");
+        $((".btn-outline-primary")).click();
+
+        String zip = $(("#zip-code")).getCssValue("background-color");
+        System.out.println("Цвет: " + zip);
+
+        String mail = $(("#e-mail")).getCssValue("background-color");
+        System.out.println("Цвет: " + mail);
+
+        String phone = $(("#phone")).getCssValue("background-color");
+        System.out.println("Цвет: " + phone);
+
+        Selenide.closeWebDriver();
     }
 }
