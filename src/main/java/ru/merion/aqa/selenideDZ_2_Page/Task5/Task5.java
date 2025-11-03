c22efd6f851375c86742cd6743009c2f5057bd3f
Andrey_O <a_reshkin@inbox.ru>
2025-11-03 22:49:55 +0300

    Update Project: - [WebDriver DZ_1 - PageObject]

--- src/main/java/ru/merion/aqa/selenideDZ_2_Page/Task5/Task5.java
+++ src/main/java/ru/merion/aqa/selenideDZ_2_Page/Task5/Task5.java
@@ -17,12 +17,32 @@ package ru.merion.aqa.selenideDZ_2_Page.Task5;
 Дождаться результата. Вывести его в консоль.
  */
 
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class Task5 {
     public static void main(String[] args) {
+        int timeout = 10;
         open("https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html");
-        $("");
+        $(("#delay")).val(Integer.toString(timeout));
+
+        SelenideElement keyboard = $(".keys");
+
+        keyboard.$x(("*[text() = '7']")).click();
+        keyboard.$x(("*[text() = '+']")).click();
+        keyboard.$x(("*[text() = '8']")).click();
+        keyboard.$x(("*[text() = '=']")).click();
+
+        $(("#spinner")).shouldNotBe(visible, Duration.ofSeconds(timeout+1));
+        String result = $((".screen")).text();
+        System.out.println("Результат: " + result);
+
+        Selenide.closeWebDriver();
     }
 }
