efa1d60fb60ffd8c5c48718236b6b4ce7c8a1d25
Andrey_O <a_reshkin@inbox.ru>
2025-11-05 21:49:32 +0300

    Update Project: - [WebDriver DZ_1 - PageObject]

---
+++ src/main/java/ru/merion/aqa/selenideDZ_2_Page/Task1/AjaxPage.java
@@ -0,0 +1,21 @@
+package ru.merion.aqa.selenideDZ_2_Page.Task1;
+import static com.codeborne.selenide.Selenide.$;
+
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
+
+public class AjaxPage {
+    private final SelenideElement button = $("ajaxButton");
+    private final SelenideElement content = $("content p");
+
+    public void open() {
+        Selenide.open("http://uitestingplayground.com/ajax");
+    }
+    public void clickTheButton() {
+        button.click();
+    }
+
+    public String getContent() {
+        return content.getText();
+    }
+}
