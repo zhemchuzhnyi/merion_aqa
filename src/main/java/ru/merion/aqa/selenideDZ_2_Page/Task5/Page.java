c0805511f5fe1af4a4103f1df22fd08f6ca2e4c8
Andrey_O <a_reshkin@inbox.ru>
2025-11-07 22:16:03 +0300

    Update Project: - [WebDriver DZ_1 - PageObject]

--- src/main/java/ru/merion/aqa/selenideDZ_2_Page/Task5/Page.java
+++ src/main/java/ru/merion/aqa/selenideDZ_2_Page/Task5/Page.java
@@ -1,10 +1,15 @@
 package ru.merion.aqa.selenideDZ_2_Page.Task5;
 
+import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 
 import static com.codeborne.selenide.Selenide.$;
 
 public class Page {
     private final SelenideElement keyboard = $(".keys");
+
+    public void open() {
+        Selenide.open("https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html");
+    }
 
 }
