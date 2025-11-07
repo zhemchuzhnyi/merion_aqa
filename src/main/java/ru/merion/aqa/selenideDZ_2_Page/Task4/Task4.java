f04b8648efc58aca0211864765de79288c73c1bf
Andrey_O <a_reshkin@inbox.ru>
2025-11-07 22:19:12 +0300

    Update Project: - [WebDriver DZ_1 - PageObject]

--- src/main/java/ru/merion/aqa/selenideDZ_2_Page/Task4/Task4.java
+++ src/main/java/ru/merion/aqa/selenideDZ_2_Page/Task4/Task4.java
@@ -43,5 +43,8 @@ public class Task4 {
         System.out.println("Цвет: " + page.getZipCss());
         System.out.println("Цвет: " + page.getMailCss());
         System.out.println("Цвет: " + page.getPhoneCss());
+
+        Selenide.closeWebDriver();
+
     }
 }
