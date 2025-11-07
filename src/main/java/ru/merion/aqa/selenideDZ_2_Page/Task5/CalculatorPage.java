e7c03f9254f4ec71965bac0b9c5c60e8c0dee2f9
Andrey_O <a_reshkin@inbox.ru>
2025-11-07 22:22:55 +0300

    Update Project: - [WebDriver DZ_1 - PageObject]

--- src/main/java/ru/merion/aqa/selenideDZ_2_Page/Task5/CalculatorPage.java
+++ src/main/java/ru/merion/aqa/selenideDZ_2_Page/Task5/CalculatorPage.java
@@ -8,13 +8,13 @@ import java.time.Duration;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 
-public class Page {
+public class CalculatorPage {
     private final SelenideElement keyboard = $(".keys");
-    private final SelenideElement deelay = $(("#delay")).val(Integer.toString(timeout));
-    private final SelenideElement spinner = $(("#spinner")).shouldNotBe(visible, Duration.ofSeconds(timeout+1));
+    private int delay = 5;
 
-    public void open() {
+    public CalculatorPage open() {
         Selenide.open("https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html");
+        return this;
     }
 
 }
