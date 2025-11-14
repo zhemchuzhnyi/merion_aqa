bbca1da4f3696366602a1e7b40a4ff1bea97e148
Andrey_O <a_reshkin@inbox.ru>
2025-11-14 17:04:41 +0300

    Update Project: - [WebDriver DZ_1 - PageObject]

--- src/main/java/ru/merion/aqa/selenideDZ_2_Page/Task5/Task5.java
+++ src/main/java/ru/merion/aqa/selenideDZ_2_Page/Task5/Task5.java
@@ -42,5 +42,8 @@ public class Task5 {
         $(("#spinner")).shouldNotBe(visible, Duration.ofSeconds(timeout+1));
         String result = $((".screen")).text();
         System.out.println("Результат: " + result);
+
+        Selenide.closeWebDriver();
+
     }
 }
