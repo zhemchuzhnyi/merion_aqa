bc7c4f507eb9c96145e85e21c1d4f1902ecc2bac
Andrey_O <a_reshkin@inbox.ru>
2025-11-18 23:29:01 +0300

    Update Project: - [WebDriver DZ_1 - PageObject]

--- src/main/java/ru/merion/aqa/selenideDZ_2_Page/Task6/Page.java
+++ src/main/java/ru/merion/aqa/selenideDZ_2_Page/Task6/Page.java
@@ -1,5 +1,47 @@
 package ru.merion.aqa.selenideDZ_2_Page.Task6;
 
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.Set;
+
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
+
 public class Page {
 
+    public static void main(String[] args) {
+        Configuration.browserCapabilities = new ChromeOptions().addArguments("--guest");
+
+        Set<String> itemNames = Set.of("Sauce Labs Backpack", "Sauce Labs Bolt T-Shirt", "Sauce Labs Onesie");
+
+        open("https://www.saucedemo.com/");
+        $(("#user-name")).val("standard_user");
+        $(("#password")).val("secret_sauce");
+        $(("#login-button")).click();
+
+        $$(".inventory_item").forEach(item -> {
+            String productName = item.find(".inventory_item_name").text();
+            if (itemNames.contains(productName)) {
+                item.find("button").click();
+            }
+        });
+
+        open("https://www.saucedemo.com/cart.html");
+        $("#checkout").click();
+
+        $("#first-name").val("Ivan");
+        $("#last-name").val("Ivanov");
+        $("#postal-code").val("12345");
+        $("#continue").click();
+
+        String total = $(".summary_total_label").getText();
+        System.out.println(total);
+
+
+        Selenide.closeWebDriver();
+    }
 }
+
+
