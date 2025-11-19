c5d88c8f382b1cfc99ba5849a5814c80430571a7
Andrey_O <a_reshkin@inbox.ru>
2025-11-19 22:10:10 +0300

    Update Project: - [WebDriver DZ_1 - PageObject]

--- src/main/java/ru/merion/aqa/selenideDZ_2_Page/Task6/CatalogPage.java
+++ src/main/java/ru/merion/aqa/selenideDZ_2_Page/Task6/CatalogPage.java
@@ -4,12 +4,12 @@ import java.util.Collection;
 import static com.codeborne.selenide.Selenide.*;
 
 public class CatalogPage {
-    private final ElementsCollection items = $$(".inventory-item");
+    private final ElementsCollection items = $$(".inventory_item");
 
     public void addItems(Collection<String> itemNames) {
         items.forEach(item -> {
-            String productName = item.find(".inventory-item-name").text();
-            if(itemNames.contains(productName)) {
+            String productName = item.find(".inventory_item_name").text();
+            if (itemNames.contains(productName)) {
                 item.find("button").click();
             }
         });
