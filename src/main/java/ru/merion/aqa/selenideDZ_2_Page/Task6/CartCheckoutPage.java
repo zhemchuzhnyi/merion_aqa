e38d1e7c62daac43596897930a46b4d1ac59aa63
Andrey_O <a_reshkin@inbox.ru>
2025-11-19 21:36:08 +0300

    Update Project: - [WebDriver DZ_1 - PageObject]

--- src/main/java/ru/merion/aqa/selenideDZ_2_Page/Task6/CartCheckoutPage.java
+++ src/main/java/ru/merion/aqa/selenideDZ_2_Page/Task6/CartCheckoutPage.java
@@ -1,5 +1,14 @@
 package ru.merion.aqa.selenideDZ_2_Page.Task6;
 
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selenide.$;
+
 public class CartCheckoutPage {
+    private final SelenideElement firstNameInput = $("#first-name");
+    private final SelenideElement lastNameInput = $("#last-name");
+    private final SelenideElement postalCode = $("#postal-code");
+    private final SelenideElement continueButton = $("#continue");
+    private final SelenideElement pricrText = $("[data-test=total-label]");
 
 }
