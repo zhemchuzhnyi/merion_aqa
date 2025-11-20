cd70acd76e63f076a88cdde2071278a0eebabacc
Andrey_O <a_reshkin@inbox.ru>
2025-11-20 22:28:11 +0300

    Update Project: - [WebDriver DZ_1 - PageObject]

--- src/main/java/ru/merion/aqa/selenideDZ_2_Page/Task5/Task5.java
+++ src/main/java/ru/merion/aqa/selenideDZ_2_Page/Task5/Task5.java
@@ -28,20 +28,16 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class Task5 {
     public static void main(String[] args) {
-        int timeout = 10;
-        open("https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html");
-        $(("#delay")).val(Integer.toString(timeout));
+        CalculatorPage calculator = new CalculatorPage().open();
 
-        SelenideElement keyboard = $(".keys");
+        calculator.setDelay(10);
+        calculator.press_7();
+        calculator.press_plus();
+        calculator.press_8();
+        calculator.press_eq();
 
-        keyboard.$x(("*[text() = '7']")).click();
-        keyboard.$x(("*[text() = '+']")).click();
-        keyboard.$x(("*[text() = '8']")).click();
-        keyboard.$x(("*[text() = '=']")).click();
-
-        $(("#spinner")).shouldNotBe(visible, Duration.ofSeconds(timeout+1));
-        String result = $((".screen")).text();
-        System.out.println("Результат: " + result);
+        String result = calculator.getResult();
+        System.out.println(result);
 
         Selenide.closeWebDriver();
 
