3dae34bcb97a1e802a58af4c8c809fb4b0cb1416
Andrey_O <a_reshkin@inbox.ru>
2025-11-20 22:24:40 +0300

    Update Project: - [WebDriver DZ_1 - PageObject]

--- src/main/java/ru/merion/aqa/selenideDZ_2_Page/Task4/Task4.java
+++ src/main/java/ru/merion/aqa/selenideDZ_2_Page/Task4/Task4.java
@@ -35,14 +35,26 @@ import com.codeborne.selenide.Selenide;
 
 public class Task4 {
     public static void main(String[] args) {
-        Page page = new Page();
-        page.open();
-        page.setForm();
-        page.clickButton();
 
-        System.out.println("Цвет: " + page.getZipCss());
-        System.out.println("Цвет: " + page.getMailCss());
-        System.out.println("Цвет: " + page.getPhoneCss());
+        RegisterPage page = new RegisterPage().open();
+
+        page.set("first-name", "Иван");
+        page.set("last-name", "Петров");
+        page.set("job-position", "QA");
+        page.set("address", "Ленина 55-3");
+        page.set("city", "Москва");
+        page.set("country", "Россия");
+        page.set("company", "Merion");
+
+        page.submitForm();
+
+        String phone_bg = page.getCssProperty("#phone", "background-color");
+        String email_bg = page.getCssProperty("#e-mail", "background-color");
+        String zip_code_bg = page.getBgColor("#zip-code");
+
+        System.out.println(zip_code_bg);
+        System.out.println(email_bg);
+        System.out.println(phone_bg);
 
         Selenide.closeWebDriver();
     }
