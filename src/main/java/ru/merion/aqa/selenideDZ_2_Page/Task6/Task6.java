8ce8bb55c84d76fb0413eca8304bc906aae9b97a
Andrey_O <a_reshkin@inbox.ru>
2025-11-21 21:57:11 +0300

    Update Project: - [WebDriver DZ_1 - PageObject]

--- src/main/java/ru/merion/aqa/selenideDZ_2_Page/Task6/Task6.java
+++ src/main/java/ru/merion/aqa/selenideDZ_2_Page/Task6/Task6.java
@@ -48,8 +48,6 @@ public class Task6 {
                 .setContactData("Иван", "Иванов", "123457")
                 .getTotalPrice();
 
-        System.out.println(total);
-
         Selenide.closeWebDriver();
     }
 }
