4085404814a739a5fa712d49ed0b69ca21e56f15
Andrey_O <a_reshkin@inbox.ru>
2025-11-24 21:41:09 +0300

    Update Project: - [@ - Нотации]

--- src/test/java/lesson_11/LabirintTest.java
+++ src/test/java/lesson_11/LabirintTest.java
@@ -9,7 +9,10 @@ import ru.merion.aqa.lesson7.page.ResultPage;
 public class LabirintTest {
 
     public static void main(String[] args) {
-
+        test1();
+        test2();
+    }
+    public static void test1(){
         WebDriver driver = WDFactory.create("chrome");
 
         MainPage mainPage = new MainPage(driver);
@@ -25,14 +28,35 @@ public class LabirintTest {
         System.out.println("iconText: " + iconText);
 
         CartPage cartPage = resultPage.header.clickCartIcon();
-
-        String price = cartPage.getCartCounter();
         String counter = cartPage.getCartPrice();
 
-        System.out.println("price : " + price);
         System.out.println("counter: " + counter);
 
         driver.quit();
+    }
 
+    public static void test2(){
+        WebDriver driver = WDFactory.create("chrome");
+
+        MainPage mainPage = new MainPage(driver);
+
+        mainPage.open();
+
+
+        ResultPage resultPage = mainPage.header.searchFor("uqwyegwqh");
+        String msg = resultPage.getEmptyResultMessage();
+        System.out.println("msg = " + msg);
+
+        resultPage.addAllItemsToCart();
+
+        String iconText = resultPage.header.getIconText();
+        System.out.println("iconText: " + iconText);
+
+        CartPage cartPage = resultPage.header.clickCartIcon();
+        String counter = cartPage.getCartPrice();
+
+        System.out.println("counter: " + counter);
+
+        driver.quit();
     }
 }
