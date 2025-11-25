4382ae6ed8c01d33abc17161cde94169a8ba877d
Andrey_O <a_reshkin@inbox.ru>
2025-11-25 22:33:52 +0300

    Update Project: - [@ - Нотации]

--- src/test/java/ru/merion/aqa/lesson_11/LabirintTest.java
+++ src/test/java/ru/merion/aqa/lesson_11/LabirintTest.java
@@ -1,6 +1,7 @@
 package ru.merion.aqa.lesson_11;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
 import ru.merion.aqa.lesson7.WDFactory;
 import ru.merion.aqa.lesson7.page.CartPage;
 import ru.merion.aqa.lesson7.page.MainPage;
@@ -17,7 +18,7 @@ public class LabirintTest {
         WebDriver driver = WDFactory.create("chrome");
 
         // Создаём объект главной страницы и передаём в него драйвер
-        MainPage mainPage = new MainPage(driver);
+        MainPage mainPage = PageFactory.initElements(driver, MainPage.class);
         // Открываем главную страницу сайта Лабиринт
         mainPage.open();
 
@@ -48,14 +49,14 @@ public class LabirintTest {
         WebDriver driver = WDFactory.create("chrome");
 
         // Создаём объект главной страницы и передаём в него драйвер
-        MainPage mainPage = new MainPage(driver);
+        MainPage mainPage = PageFactory.initElements(driver, MainPage.class);
         // Открываем главную страницу сайта Лабиринт
         mainPage.open();
 
         // Выполняем поиск по ключевому слову "Java"
-        ResultPage resultPage = mainPage.header.searchFor("Java");
+        ResultPage resultPage = mainPage.header.searchFor("    @@@@   ");
         // Добавляем все доступные товары из результатов поиска в корзину
-        resultPage.addAllItemsToCart();
+        resultPage.getEmptyResultMessage();
         // Проверяем счётчик товаров на иконке корзины
         String iconText = resultPage.header.getIconText();
         System.out.println("iconText: " + iconText);
