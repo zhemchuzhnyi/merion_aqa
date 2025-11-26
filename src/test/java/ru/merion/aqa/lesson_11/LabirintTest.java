fe8f4abb9d86d11ac9c88d62cf69d168851f57ee
Andrey_O <a_reshkin@inbox.ru>
2025-11-26 22:36:12 +0300

    Update Project: - [@ - Нотации]

--- src/test/java/ru/merion/aqa/lesson_11/LabirintTest.java
+++ src/test/java/ru/merion/aqa/lesson_11/LabirintTest.java
@@ -7,22 +7,31 @@ import ru.merion.aqa.lesson7.page.CartPage;
 import ru.merion.aqa.lesson7.page.MainPage;
 import ru.merion.aqa.lesson7.page.ResultPage;
 
+import java.time.Duration;
+
 public class LabirintTest {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         test_1();
         test_2();
     }
 
-    public static void test_1() {
-        System.out.println("Начинаем проводить Тест_1");
+    public static void test_1() throws InterruptedException {
+        System.out.println("Начинаем проводить позитивный тест на поиск по сайту");
         WebDriver driver = WDFactory.create("chrome");
+
+// Установить неявное ожидание для всех элементов
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
         MainPage mainPage = PageFactory.initElements(driver, MainPage.class);
         mainPage.open();
 
         ResultPage resultPage = mainPage.header.searchFor("Java");
         resultPage.addAllItemsToCart();
+
+// Небольшая пауза после добавления всех товаров
+        Thread.sleep(2000); // 2 секунды
+
         String iconText = resultPage.header.getIconText();
         if (iconText.equals("37")){
             System.out.println("Проверили текст иконки");
@@ -31,7 +40,9 @@ public class LabirintTest {
         }
 
         CartPage cartPage = resultPage.header.clickCartIcon();
-        String counter = cartPage.getCartPrice();
+        Thread.sleep(3000); // 2 сек для загрузки корзины
+
+        String counter = cartPage.getCartCounter();
         if (counter.equals("37")){
             System.out.println("Проверили текст корзины");
         } else {
@@ -42,7 +53,7 @@ public class LabirintTest {
     }
 
     public static void test_2() {
-        System.out.println("\nНачинаем проводить Тест_2");
+        System.out.println("\nНачинаем проводить негативный тест на поиск по сайту");
         WebDriver driver = WDFactory.create("chrome");
 
         MainPage mainPage = PageFactory.initElements(driver, MainPage.class);
