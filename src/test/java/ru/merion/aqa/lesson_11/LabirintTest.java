17c7a5aec8fefb6ad90d22612c818542d84d8f52
Andrey_O <a_reshkin@inbox.ru>
2025-11-29 15:55:32 +0300

    Update Project: - [@ - Нотации]

--- src/test/java/ru/merion/aqa/lesson_11/LabirintTest.java
+++ src/test/java/ru/merion/aqa/lesson_11/LabirintTest.java
@@ -54,5 +54,7 @@ public class LabirintTest {
 
     private MainPage openMainPage(WebDriver driver) {
         MainPage mainPage = PageFactory.initElements(driver, MainPage.class);
+        mainPage.open();
+        return mainPage;
     }
 }
