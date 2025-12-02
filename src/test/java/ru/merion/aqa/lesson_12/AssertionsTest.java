69ff303c4fd3547bafd207ef900231b1798aa0ff
Andrey_O <a_reshkin@inbox.ru>
2025-12-02 22:25:12 +0300

    Update Project: - [@ - Нотации]

--- src/test/java/ru/merion/aqa/lesson_12/AssertionsTest.java
+++ src/test/java/ru/merion/aqa/lesson_12/AssertionsTest.java
@@ -2,8 +2,9 @@ package ru.merion.aqa.lesson_12;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class AssertionsTest {
 
@@ -16,15 +17,15 @@ public class AssertionsTest {
     @Test
     public void arraysEqualityDemo() {
         int[] arrA = {1,2,3,4,5};
-        int[] arrB = {1,2,3,4,5};
+        int[] arrB = {1,2,3,4,5,};
         assertArrayEquals(arrA, arrB);
     }
 
     @Test
     public void arraysNonEqualityDemo() {
         int[] arrA = {1,2,3,4,5};
-        int[] arrB = {1,2,3,4,9};
-        assertArrayEquals(arrA, arrB);
+        int[] arrB = {1,2,3,4,5,9};
+        assertFalse(Arrays.equals(arrA, arrB));
     }
 
 }
