e1c53cb53e2f7a71fd95628252ff4f9510d22776
Andrey_O <a_reshkin@inbox.ru>
2025-12-02 22:08:52 +0300

    Update Project: - [@ - Нотации]

--- src/test/java/ru/merion/aqa/lesson_12/AssertionsTest.java
+++ src/test/java/ru/merion/aqa/lesson_12/AssertionsTest.java
@@ -2,9 +2,14 @@ package ru.merion.aqa.lesson_12;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class AssertionsTest {
     @Test
-    public void equalsAssert() {
+    public void eqAssert() {
+        int actual = 2+2;
+        int expected = 4;
+        assertEquals(expected, actual);
 
     }
 }
