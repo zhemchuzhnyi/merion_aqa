f215102137788a162575f510c9eb11a71e60a11f
Andrey_O <a_reshkin@inbox.ru>
2025-12-02 22:19:40 +0300

    Update Project: - [@ - Нотации]

--- src/test/java/ru/merion/aqa/lesson_12/AssertionsTest.java
+++ src/test/java/ru/merion/aqa/lesson_12/AssertionsTest.java
@@ -5,11 +5,18 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AssertionsTest {
+
     @Test
     public void eqAssert() {
         int actual = 2+2;
-        int expected = 4;
-        assertEquals(expected, actual);
-
+        int expected = 5;
+        assertEquals(expected, actual, "Математика сломалась");
     }
+    @Test
+    public void arraysEqualityDemo() {
+        int[] arrA = {1,2,3,4,5};
+        int[] arrB = {1,2,3,4,5};
+        assertEquals(arrA, arrB);
+    }
+
 }
