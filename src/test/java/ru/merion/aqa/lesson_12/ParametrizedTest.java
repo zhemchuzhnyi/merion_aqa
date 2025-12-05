b5108cdefaa59740654c3ad0b2445c034d98afad
Andrey_O <a_reshkin@inbox.ru>
2025-12-05 19:22:46 +0300

    Update Project: - [@ - Нотации / Параметризация]

--- src/test/java/ru/merion/aqa/lesson_12/ParametrizedTest.java
+++ src/test/java/ru/merion/aqa/lesson_12/ParametrizedTest.java
@@ -60,7 +60,8 @@ public class ParametrizedTest {
         driver.findElement(By.cssSelector("[name=Password]")).sendKeys(pass);
         driver.findElement(By.cssSelector("#login")).click();
 
-
+        String msg = driver.findElement(By.cssSelector("#loginstatus")).getText();
+        assertEquals("Invalid username/password", msg);
     }
 
 }
