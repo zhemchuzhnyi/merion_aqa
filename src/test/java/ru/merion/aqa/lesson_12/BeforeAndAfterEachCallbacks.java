03ca2bb2c873f8171e42d64dcb1b895a26d482c5
Andrey_O <a_reshkin@inbox.ru>
2025-12-08 23:57:54 +0300

    Update Project: - [@ - Нотации / Параметризация]

---
+++ src/test/java/ru/merion/aqa/lesson_12/BeforeAndAfterEachCallbacks.java
@@ -0,0 +1,17 @@
+package ru.merion.aqa.lesson_12;
+
+import org.junit.jupiter.api.extension.AfterEachCallback;
+import org.junit.jupiter.api.extension.BeforeEachCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+
+public class BeforeAndAfterEachCallbacks implements BeforeEachCallback, AfterEachCallback {
+    @Override
+    public void afterEach(ExtensionContext context) throws Exception {
+
+    }
+
+    @Override
+    public void beforeEach(ExtensionContext context) throws Exception {
+
+    }
+}
