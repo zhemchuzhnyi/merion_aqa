6e08b1a4ae1e95597ff1d9cad6ed4b307e0b137e
Andrey_O <a_reshkin@inbox.ru>
2025-12-09 01:04:35 +0300

    Update Project: - [@ - Нотации / Параметризация / Расширения]

--- src/test/java/ru/merion/aqa/lesson_12/ext/BeforeAndAfterEachCallbacks.java
+++ src/test/java/ru/merion/aqa/lesson_12/ext/BeforeAndAfterEachCallbacks.java
@@ -3,9 +3,7 @@ package ru.merion.aqa.lesson_12.ext;
 import org.junit.jupiter.api.extension.*;
 import org.openqa.selenium.WebDriver;
 import ru.merion.aqa.WebDriverFactory;
-
 import java.lang.reflect.Field;
-import java.util.Optional;
 
 public class BeforeAndAfterEachCallbacks implements BeforeEachCallback, AfterEachCallback {
 
@@ -13,25 +11,20 @@ public class BeforeAndAfterEachCallbacks implements BeforeEachCallback, AfterEac
 
     @Override
     public void beforeEach(ExtensionContext context) throws Exception {
-        System.out.println("Начинаем выполнять");
-        Optional<Object> testInstance = context.getTestInstance();
-        if (testInstance.isPresent() && testInstance.get().getClass().isAnnotationPresent(InjectWebDriver.class)) {
-            driver = WebDriverFactory.create("chrome");
-
-            // Используем рефлексию для установки driver в любой тестовый класс
-            try {
-                Field driverField = testInstance.get().getClass().getField("driver");
-                driverField.setAccessible(true);
-                driverField.set(testInstance.get(), driver);
-            } catch (NoSuchFieldException e) {
-                System.err.println("Поле driver не найдено в классе " + testInstance.get().getClass().getName());
+        Object testInstance = context.getTestInstance().get();
+        if (testInstance.getClass().isAnnotationPresent(InjectWebDriver.class)) {
+            Field[] fields = testInstance.getClass().getFields();
+            for (Field field : fields) {
+                if (field.getType().equals(WebDriver.class)) {
+                    driver = WebDriverFactory.create("chrome");
+                    field.set(testInstance, driver);
+                }
             }
         }
     }
 
     @Override
     public void afterEach(ExtensionContext context) throws Exception {
-        System.out.println("Закончили выполнять");
         if (this.driver != null) {
             driver.quit();
             driver = null;
