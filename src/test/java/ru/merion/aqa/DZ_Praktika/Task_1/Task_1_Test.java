b1151059dae2fb4a5b45eb30abe6c73d9c9ddfce
Andrey_O <a_reshkin@inbox.ru>
2025-12-11 23:04:08 +0300

    Update Project: - [Praktics DZ]

--- src/test/java/ru/merion/aqa/DZ_Praktika/Task_1/Task_1_Test.java
+++ src/test/java/ru/merion/aqa/DZ_Praktika/Task_1/Task_1_Test.java
@@ -37,10 +37,4 @@ public class Task_1_Test {
         }
     }
 
-    @Test
-    public void iCanRenameTheButton() {
-        String text = new TextInputPage(driver).open().setButtonName(BUTTON_NAME).getButtonText();
-        assertEquals(BUTTON_NAME, text);
-    }
-
 }
