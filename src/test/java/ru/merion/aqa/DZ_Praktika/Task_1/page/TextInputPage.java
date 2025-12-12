d3bb0119123b72866c001faee5059f31535b43dd
Andrey_O <a_reshkin@inbox.ru>
2025-12-12 22:53:31 +0300

    Update Project: - [Praktics DZ]

--- src/test/java/ru/merion/aqa/DZ_Praktika/Task_1/page/TextInputPage.java
+++ src/test/java/ru/merion/aqa/DZ_Praktika/Task_1/page/TextInputPage.java
@@ -20,9 +20,4 @@ public class TextInputPage {
         driver.findElement(By.cssSelector("#updatingButton")).click();
         return this;
     }
-
-    public String getButtonText() {
-        return driver.findElement(By.cssSelector("#updatingButton")).getText();
-
-    }
 }
