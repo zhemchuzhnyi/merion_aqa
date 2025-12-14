d15be19bb901aca00f8f572a4ca87d04f2a1e111
Andrey_O <a_reshkin@inbox.ru>
2025-12-14 15:44:24 +0300

    Update Project: - [Praktics DZ]

---
+++ src/test/java/ru/merion/aqa/DZ_Praktika/Task_2/page/RegisterPage.java
@@ -0,0 +1,12 @@
+package ru.merion.aqa.DZ_Praktika.Task_2.page;
+
+import org.openqa.selenium.WebDriver;
+
+public class RegisterPage {
+    private final WebDriver driver;
+
+    public RegisterPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+}
