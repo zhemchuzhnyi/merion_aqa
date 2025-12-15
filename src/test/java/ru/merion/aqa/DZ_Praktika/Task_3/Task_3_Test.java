14a19b3366769944d1bce441ea6d7d550174ddd2
Andrey_O <a_reshkin@inbox.ru>
2025-12-15 23:03:25 +0300

    Update Project: - [Praktics DZ]

--- src/test/java/ru/merion/aqa/DZ_Praktika/Task_3/Task_3_Test.java
+++ src/test/java/ru/merion/aqa/DZ_Praktika/Task_3/Task_3_Test.java
@@ -1,5 +1,11 @@
 package ru.merion.aqa.DZ_Praktika.Task_3;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 /**
  * Скрипт на калькулятор
  *
@@ -18,4 +24,20 @@ package ru.merion.aqa.DZ_Praktika.Task_3;
  */
 
 public class Task_3_Test {
+    private WebDriver driver;
+
+    @BeforeEach
+    public void open() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    public void close() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test
+
 }
