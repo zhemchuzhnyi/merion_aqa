793454b66514924083335bbcd98b15e8f3600b2c
Andrey_O <a_reshkin@inbox.ru>
2025-12-17 23:12:15 +0300

    Update Project: - [Praktics DZ]

---
+++ src/test/java/ru/merion/aqa/DZ_Praktika/Task_4/page/CartCheckoutPage.java
@@ -0,0 +1,11 @@
+package ru.merion.aqa.DZ_Praktika.Task_4.page;
+
+import org.openqa.selenium.WebDriver;
+
+public class CartCheckoutPage {
+    private final WebDriver driver;
+
+    public CartCheckoutPage(WebDriver driver) {
+        this.driver = driver;
+    }
+}
