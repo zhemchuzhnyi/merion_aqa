56a6563586f9862f291c577f7be853fb5aca34f6
Andrey_O <a_reshkin@inbox.ru>
2025-12-23 21:26:53 +0300

    Update Project: - [jUnit extentions]

---
+++ src/test/java/ru/merion/aqa/lesson_13/TestWatcherDemo.java
@@ -0,0 +1,5 @@
+package ru.merion.aqa.lesson_13;
+
+public class TestWatcherDemo {
+
+}
---
+++ src/test/java/ru/merion/aqa/lesson_13/ext/MyTestReporter.java
@@ -0,0 +1,6 @@
+package ru.merion.aqa.lesson_13.ext;
+
+import org.junit.jupiter.api.extension.TestWatcher;
+
+public class MyTestReporter implements TestWatcher {
+}
