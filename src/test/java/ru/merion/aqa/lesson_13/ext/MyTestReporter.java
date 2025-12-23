bdd3e2ff4d6a0c93255cfddc356ecc7a09060984
Andrey_O <a_reshkin@inbox.ru>
2025-12-23 21:43:41 +0300

    Update Project: - [jUnit extentions]

--- src/test/java/ru/merion/aqa/lesson_13/ext/MyTestReporter.java
+++ src/test/java/ru/merion/aqa/lesson_13/ext/MyTestReporter.java
@@ -2,10 +2,18 @@ package ru.merion.aqa.lesson_13.ext;
 
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.extension.TestWatcher;
+import org.opentest4j.AssertionFailedError;
 
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 public class MyTestReporter implements TestWatcher {
+
+    private final Map<String, Method> greenTests = new HashMap<>();
+    private final Map<String, Method> redTests = new HashMap<>();
+    private final Map<String, Method> yellowTests = new HashMap<>();
 
     @Override
     public void testDisabled(ExtensionContext context, Optional<String> reason) {
@@ -16,6 +24,7 @@ public class MyTestReporter implements TestWatcher {
     public void testSuccessful(ExtensionContext context) {
         TestWatcher.super.testSuccessful(context);
         System.out.println(context.getDisplayName());
+        greenTests.put(context.getDisplayName(), context.getRequiredTestMethod());
     }
 
     @Override
@@ -27,5 +36,9 @@ public class MyTestReporter implements TestWatcher {
     public void testFailed(ExtensionContext context, Throwable cause) {
         TestWatcher.super.testFailed(context, cause);
         System.out.println(context.getDisplayName());
+
+        if (cause instanceof AssertionFailedError) {
+            yellowTests.put(context.getDisplayName(), context.getRequiredTestMethod());
+        }
     }
 }
