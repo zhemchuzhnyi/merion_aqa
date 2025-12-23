630a97d5b5d5c5eb260483fdee725e6dee97f7d9
Andrey_O <a_reshkin@inbox.ru>
2025-12-24 00:46:26 +0300

    Update Project: - [jUnit extentions]

--- src/test/java/ru/merion/aqa/lesson_13/ext/DriverResolver.java
+++ src/test/java/ru/merion/aqa/lesson_13/ext/DriverResolver.java
@@ -1,18 +1,26 @@
 package ru.merion.aqa.lesson_13.ext;
 
-import org.junit.jupiter.api.extension.ExtensionContext;
-import org.junit.jupiter.api.extension.ParameterContext;
-import org.junit.jupiter.api.extension.ParameterResolutionException;
-import org.junit.jupiter.api.extension.ParameterResolver;
+import org.junit.jupiter.api.extension.*;
+import org.openqa.selenium.WebDriver;
+import ru.merion.aqa.WebDriverFactory;
 
-public class DriverResolver implements ParameterResolver {
+public class DriverResolver implements ParameterResolver, AfterAllCallback {
     @Override
     public boolean supportsParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException {
-        return false;
+        return true;
+    }
+
+    private WebDriver driver;
+    @Override
+    public Object resolveParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException {
+        driver = WebDriverFactory.create("chrome");
+        return driver;
     }
 
     @Override
-    public Object resolveParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException {
-        return null;
+    public void afterAll(ExtensionContext context) throws Exception {
+        if (driver != null) {
+            driver.quit();
+        }
     }
 }
