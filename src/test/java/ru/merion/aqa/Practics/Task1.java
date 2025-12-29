3162c5d0b2630d9ab9f7122a358c038d944faeb9
Andrey_O <a_reshkin@inbox.ru>
2025-12-29 23:03:11 +0300

    Update Project: - [jUnit extentions]

--- src/test/java/ru/merion/aqa/Practics/Task1.java
+++ src/test/java/ru/merion/aqa/Practics/Task1.java
@@ -2,16 +2,19 @@ package ru.merion.aqa.Practics;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Task1 {
     public static void main(String[] args) {
+
         WebDriver driver = new ChromeDriver();
         driver.get("https://the-internet.herokuapp.com/login");
         driver.findElement(By.cssSelector(".username")).sendKeys("tomsmith");
         driver.findElement(By.cssSelector(".password")).sendKeys("SuperSecretPassword!");
         driver.findElement(By.cssSelector(".fa-fa-2x-fa-sign-in")).click();
+
+        driver.findElement(By.cssSelector(".subheader")).getText();
+
 
     }
 }
