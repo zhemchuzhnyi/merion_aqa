4f110ba90df193ec844a986804ddd460d4ae274a
Andrey_O <a_reshkin@inbox.ru>
2025-12-29 23:39:40 +0300

    Update Project: - [jUnit extentions]

--- src/test/java/ru/merion/aqa/Practics/Task2.java
+++ src/test/java/ru/merion/aqa/Practics/Task2.java
@@ -1,5 +1,6 @@
 package ru.merion.aqa.Practics;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -13,5 +14,14 @@ public class Task2 {
     public void open() {
         driver = new ChromeDriver();
     }
+
+    @AfterEach
+    public void close() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+
 
 }
