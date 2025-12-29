a28453a9e5767a26579fa94ad8c194310d9a0ef5
Andrey_O <a_reshkin@inbox.ru>
2025-12-29 22:55:47 +0300

    Update Project: - [jUnit extentions]

--- src/test/java/ru/merion/aqa/Practics/Task1.java
+++ src/test/java/ru/merion/aqa/Practics/Task1.java
@@ -1,7 +1,15 @@
 package ru.merion.aqa.Practics;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Task1 {
     public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/login");
+        WebElement element = driver.findElement(By.cssSelector(""));
 
     }
 }
