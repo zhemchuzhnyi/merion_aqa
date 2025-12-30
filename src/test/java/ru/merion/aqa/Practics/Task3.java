3239a4ffae1aaecf2ec0b6de39ba56ee95550c37
Andrey_O <a_reshkin@inbox.ru>
2025-12-30 18:17:08 +0300

    Update Project: - [jUnit extentions]

--- src/test/java/ru/merion/aqa/Practics/Task3.java
+++ src/test/java/ru/merion/aqa/Practics/Task3.java
@@ -3,13 +3,21 @@ package ru.merion.aqa.Practics;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class Task3 {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.get("https://www.saucedemo.com/");
         driver.findElement(By.cssSelector("#user-name")).sendKeys("visual_user");
-        driver.findElement(By.cssSelector("#password")).sendKeys("visual_user");
+        driver.findElement(By.cssSelector("#password")).sendKeys("secret_sauce");
         driver.findElement(By.cssSelector("#login-button")).click();
+
+
+        driver.findElement(By.cssSelector("#react-burger-menu-btn")).click();
     }
 }
