1d2eda35c1f62287f1047e64f25097ee3c91a7f0
Andrey_O <a_reshkin@inbox.ru>
2025-12-31 18:01:46 +0300

    Update Project: - [jUnit extentions]

--- src/test/java/ru/merion/aqa/Practics/page/AuthPageTask3.java
+++ src/test/java/ru/merion/aqa/Practics/page/AuthPageTask3.java
@@ -1,5 +1,6 @@
 package ru.merion.aqa.Practics.page;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class AuthPageTask3 {
@@ -12,6 +13,13 @@ public class AuthPageTask3 {
     public AuthPageTask3 open() {
         driver.get("https://www.saucedemo.com/");
         return this;
+    }
+
+    public MainPageOpen login() {
+        driver.findElement(By.cssSelector("#user-name")).sendKeys("visual_user");
+        driver.findElement(By.cssSelector("#password")).sendKeys("secret_sauce");
+        driver.findElement(By.cssSelector("#login-button")).click();
+        return new MainPageOpen;
     }
 
 }
