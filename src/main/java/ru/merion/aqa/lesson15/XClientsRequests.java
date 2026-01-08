e92cd1d5a0386db28662c5ed21b7ac8b990ea3e0
Andrey_O <a_reshkin@inbox.ru>
2026-01-08 23:34:11 +0300

    lesson 15 - okhttp practice

---
+++ src/main/java/ru/merion/aqa/lesson15/XClientsRequests.java
@@ -0,0 +1,12 @@
+package ru.merion.aqa.lesson15;
+
+public class XClientsRequests {
+
+    public static final String URL = "http://51.250.26.13:8083/docs/";
+    public static final String COMPANY = "/company";
+    public static final String LOGIN = "/auth/login";
+
+    public static void main(String[] args) {
+
+    }
+}
