77cf0cd2788340f7b51857498e62aa5dffef631b
Andrey_O <a_reshkin@inbox.ru>
2026-01-10 22:52:36 +0300

    lesson 15 - Rest Api OkHttp3 practice

--- src/main/java/ru/merion/aqa/lesson15/model/AuthRequest.java
+++ src/main/java/ru/merion/aqa/lesson15/model/AuthRequest.java
@@ -1,41 +1,4 @@
 package ru.merion.aqa.lesson15.model;
 
-import java.util.Objects;
-
-public class AuthRequest {
-    public String username;
-    public String password;
-
-    public AuthRequest(String username, String password) {
-        this.username = username;
-        this.password = password;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-        AuthRequest that = (AuthRequest) o;
-        return Objects.equals(username, that.username) && Objects.equals(password, that.password);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(username, password);
-    }
+public record AuthRequest(String username, String password) {
 }
