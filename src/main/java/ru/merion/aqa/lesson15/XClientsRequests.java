a0394429f238563f52801fdb1d9f1c65f36665cb
Andrey_O <a_reshkin@inbox.ru>
2026-01-10 22:15:58 +0300

    lesson 15 - Rest Api OkHttp3 practice

--- src/main/java/ru/merion/aqa/lesson15/XClientsRequests.java
+++ src/main/java/ru/merion/aqa/lesson15/XClientsRequests.java
@@ -6,6 +6,8 @@ import java.io.IOException;
 
 public class XClientsRequests {
 
+    private static final MediaType JSON = MediaType.get("application/json");
+
     public static final String URL = "http://51.250.26.13:8083/docs/";
     public static final String COMPANY = "/company";
     public static final String LOGIN = "/auth/login";
@@ -18,6 +20,18 @@ public class XClientsRequests {
 //        Response response = client.newCall(getAllCompanies).execute();
 //        System.out.println(response.body().string());
 
+        String creds = """
+                {
+                  "username": "leonardo",
+                  "password": "leads"
+                }
+                """;
+
+        RequestBody authBody = RequestBody.create(creds, JSON);
+        Request authReq = new Request.Builder().post(authBody).url(URL + LOGIN).build();
+        Response authResp = client.newCall(authReq).execute();
+        System.out.println(authResp.body().string());
+
         String json = """
                 {
                   "name": "Merion Academy",
@@ -25,8 +39,14 @@ public class XClientsRequests {
                 }
                 """;
 
-        RequestBody reqBody = RequestBody.create(json, MediaType.get("application/json"));
-        Request createNew = new Request.Builder().post(reqBody).url(URL + COMPANY).build();
+
+        String token;
+        RequestBody reqBody = RequestBody.create(json, JSON);
+        Request createNew = new Request.Builder()
+                .post(reqBody)
+                .header("x-client-token", token)
+                .url(URL + COMPANY)
+                .build();
         Response response = client.newCall(createNew).execute();
         System.out.println(response.body().string());
 
