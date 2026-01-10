72ed47e25d7ad2254c110a8e242ebed788584c6b
Andrey_O <a_reshkin@inbox.ru>
2026-01-10 21:58:24 +0300

    lesson 15 - Rest Api OkHttp3 practice

--- src/main/java/ru/merion/aqa/lesson15/XClientsRequests.java
+++ src/main/java/ru/merion/aqa/lesson15/XClientsRequests.java
@@ -1,12 +1,27 @@
 package ru.merion.aqa.lesson15;
 
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+
 public class XClientsRequests {
 
     public static final String URL = "http://51.250.26.13:8083/docs/";
     public static final String COMPANY = "/company";
     public static final String LOGIN = "/auth/login";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        OkHttpClient client = new OkHttpClient();
+
+        Request getAllCompanies = new Request.Builder().url(URL + COMPANY).build();
+
+        Response response = client.newCall(getAllCompanies).execute();
+
+        System.out.println(response.body().string());
+
 
     }
 }
