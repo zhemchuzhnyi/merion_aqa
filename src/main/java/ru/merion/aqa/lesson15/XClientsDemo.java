188aac7bd095133af85aaff52d88f36fbcd0c8a6
Andrey_O <a_reshkin@inbox.ru>
2026-01-16 23:31:42 +0300

    lesson 15 - Rest Api OkHttp3 practice

--- src/main/java/ru/merion/aqa/lesson15/XClientsDemo.java
+++ src/main/java/ru/merion/aqa/lesson15/XClientsDemo.java
@@ -46,8 +46,6 @@ public class XClientsDemo {
         System.out.println("Активирована: " + activatedCompany);
 
         // удаление компании
-        Company companyDeleteById = service.deleteById(newCompanyID, token);
-        System.out.println(companyDeleteById);
 
     }
 }
