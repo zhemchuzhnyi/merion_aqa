38d48b0fa65d1b2c5d15b3f483a7a5e9e93bfff5
Andrey_O <a_reshkin@inbox.ru>
2026-02-03 22:53:13 +0300

    lesson 16 - Rest Api OkHttp3 practice

--- src/test/java/ru/merion/aqa/lesson_16/constract/XClientsTest.java
+++ src/test/java/ru/merion/aqa/lesson_16/constract/XClientsTest.java
@@ -1,11 +1,16 @@
 package ru.merion.aqa.lesson_16.constract;
 
+import okhttp3.OkHttpClient;
 import org.junit.jupiter.api.Test;
 
 public class XClientsTest {
 
+    public static final String URL = "http://51.250.26.13:8083/docs/#/company";
+
     @Test
     public void shouldReturnArrayOnGetCompanyList() {
+        OkHttpClient client = new OkHttpClient();
+
 
     }
 }
