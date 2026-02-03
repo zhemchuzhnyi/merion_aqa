5fdc5fcccdf3795cede8ebe6317c3454009813d4
Andrey_O <a_reshkin@inbox.ru>
2026-02-03 22:56:09 +0300

    lesson 16 - Rest Api OkHttp3 practice

--- src/test/java/ru/merion/aqa/lesson_16/constract/XClientsTest.java
+++ src/test/java/ru/merion/aqa/lesson_16/constract/XClientsTest.java
@@ -5,16 +5,22 @@ import okhttp3.Request;
 import okhttp3.Response;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class XClientsTest {
 
     public static final String URL = "http://51.250.26.13:8083/docs/#/company";
 
     @Test
-    public void shouldReturnArrayOnGetCompanyList() {
+    public void shouldReturnArrayOnGetCompanyList() throws IOException {
         OkHttpClient client = new OkHttpClient();
 
         Request request = new Request.Builder().url(URL).build();
         Response response = client.newCall(request).execute();
+
+        assertEquals(200, response.code());
 
 
     }
