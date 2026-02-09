3a5a3a05ee1bd7adbe62a0cd876c1568eb654223
oreshkin.andrey8 <oreshkin.andrey8@rwb.ru>
2026-02-09 23:13:01 +0300

    lesson 16 - Rest Api OkHttp3 practice

--- src/test/java/ru/merion/aqa/lesson_16/constract/XClientsTest.java
+++ src/test/java/ru/merion/aqa/lesson_16/constract/XClientsTest.java
@@ -21,6 +21,7 @@ public class XClientsTest {
         Response response = client.newCall(request).execute();
 
         assertEquals(200, response.code());
+        String responseBody = response.body().string();
     }
 
 }
