f937680fc0f6f595f869519c17d7257051b97330
The_araspel <a_reshkin@inbox.ru>
2026-06-25 00:13:12 +0300

    Practics

--- Merion/src/main/java/ru/merion/aqa/lesson15/model/Company.java
+++ Merion/src/main/java/ru/merion/aqa/lesson15/model/Company.java
@@ -1,7 +1,13 @@
 package ru.merion.aqa.lesson15.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record Company(int id, String name) {
+public record Company(
+        int id,
+        String name,
+        String description,
+        @JsonProperty("isActive") boolean isActive
+) {
 }
