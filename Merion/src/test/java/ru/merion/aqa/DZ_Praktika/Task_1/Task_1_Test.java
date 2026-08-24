2c1fc1679029dad7c51fd8ea8dffe177889ce761
The_araspel <a_reshkin@inbox.ru>
2026-08-24 23:17:25 +0300

    update

--- Merion/src/test/java/ru/merion/aqa/DZ_Praktika/Task_1/Task_1_Test.java
+++ Merion/src/test/java/ru/merion/aqa/DZ_Praktika/Task_1/Task_1_Test.java
@@ -41,5 +41,7 @@ public class Task_1_Test {
         String text = new TextInputPage(driver).open().setButtonName(BUTTON_NAME).getButtonText();
         assertEquals(BUTTON_NAME, text);
 
+
+
     }
 }
